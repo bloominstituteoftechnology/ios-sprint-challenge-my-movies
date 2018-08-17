4db6f003800716859f2a7c769d8e6e12450ab7d4
Spencer Curtis <spencercolecurtis@gmail.com>
2018-08-17 01:31:54 -0600

    Create starter project

---
+++ MyMovies/MyMovies/Models/MovieRepresentation.swift
@@ -0,0 +1,21 @@
+//
+//  MovieRepresentation.swift
+//  MyMovies
+//
+//  Created by Spencer Curtis on 8/17/18.
+//  Copyright © 2018 Lambda School. All rights reserved.
+//
+
+import Foundation
+
+struct MovieRepresentation: Equatable, Codable {
+    let title: String
+}
+
+/*
+ Represents the full JSON returned from searching for a movie.
+ The actual movies are in the "results" dictionary of the JSON.
+ */
+struct MovieRepresentations: Codable {
+    let results: [MovieRepresentation]
+}
