4c73ff12eef9bb1ea800a6da818ab8b566767d61
Jonathan T. Miles <jonathan.t.miles@gmail.com>
2018-08-17 09:12:39 -0700

    implements custom MovieSearch Cell, coreDataStack, local save method on the modelController.

---
+++ MyMovies/MyMovies/Cells/MovieSearchTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  MovieSearchTableViewCell.swift
+//  MyMovies
+//
+//  Created by Jonathan T. Miles on 8/17/18.
+//  Copyright © 2018 Lambda School. All rights reserved.
+//
+
+import UIKit
+
+class MovieSearchTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
---
+++ MyMovies/MyMovies/Models/Movie+Convenience.swift
@@ -0,0 +1,9 @@
+//
+//  Movie+Convenience.swift
+//  MyMovies
+//
+//  Created by Jonathan T. Miles on 8/17/18.
+//  Copyright © 2018 Lambda School. All rights reserved.
+//
+
+import Foundation
