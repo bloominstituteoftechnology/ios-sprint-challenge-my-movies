989645b94cd3888eb30d23b1d00962bb418a7d09
Jerrick Warren <Jerrick.Warren@gmail.com>
2018-11-09 08:56:34 -0800

    added CoreDataStack and Convenience File

---
+++ MyMovies/MyMovies/Models/Movie + Convenience.swift
@@ -0,0 +1,14 @@
+//
+//  Movie + Convenience.swift
+//  MyMovies
+//
+//  Created by Jerrick Warren on 11/9/18.
+//  Copyright © 2018 Lambda School. All rights reserved.
+//
+
+import Foundation
+import CoreData
+
+extension Movie {
+    
+}
