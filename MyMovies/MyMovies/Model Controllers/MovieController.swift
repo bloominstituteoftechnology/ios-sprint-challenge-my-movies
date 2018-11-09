776b0194be6e44cb34520459b569792bd9fb3f03
Jerrick Warren <Jerrick.Warren@gmail.com>
2018-11-09 14:02:40 -0800

    added movie to be encodable

--- MyMovies/MyMovies/Model Controllers/MovieController.swift
+++ MyMovies/MyMovies/Model Controllers/MovieController.swift
@@ -164,3 +164,17 @@ class MovieController {
     var searchedMovies: [MovieRepresentation] = []
     
 }
+extension Movie: Encodable {
+    enum CodingKeys: String, CodingKey {
+        case title
+        case identifier
+        case hasWatched
+    }
+    
+    public func encode(to encoder: Encoder) throws {
+        var container = encoder.container(keyedBy: CodingKeys.self)
+        
+        try container.encode(self.title, forKey: .title)
+        try container.encode(self.identifier, forKey: .identifier)
+        try container.encode(self.hasWatched, forKey: .hasWatched)
+    }}
