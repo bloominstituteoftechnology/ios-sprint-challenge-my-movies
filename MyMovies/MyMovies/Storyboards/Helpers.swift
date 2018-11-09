6d9ac27133aa421beca9f95479ea9d84f26c90de
hendoware <hendoware@gmail.com>
2018-11-09 11:11:02 -0500

    add Helpers

---
+++ MyMovies/MyMovies/Storyboards/Helpers.swift
@@ -0,0 +1,13 @@
+import Foundation
+import UIKit
+
+extension UINavigationBar {
+    
+    func shouldRemoveShadow(_ value: Bool) -> Void {
+        if value {
+            self.setValue(true, forKey: "hidesShadow")
+        } else {
+            self.setValue(false, forKey: "hidesShadow")
+        }
+    }
+}
