0dc56d3b68831e86062d57e56f18f8b6d7c14aac
Jerrick Warren <Jerrick.Warren@gmail.com>
2018-11-09 08:30:41 -0800

    updated to Swift 4.2 , added MovieTableview Cell

---
+++ MyMovies/MyMovies/View Controllers/MovieTableViewCell.swift
@@ -0,0 +1,27 @@
+//
+//  MovieTableViewCell.swift
+//  MyMovies
+//
+//  Created by Jerrick Warren on 11/9/18.
+//  Copyright © 2018 Lambda School. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class MovieTableViewCell: UITableViewCell {
+    
+    
+    @IBOutlet weak var movieLabel: UILabel!
+    
+   
+    @IBAction func saveMovie(_ sender: Any) {
+        
+        
+    }
+    
+    // outlets
+    
+    
+    
+}
