089c148752fd8206193c6921add0404151a80f03
Moses Robinson <mosesjrobinson@icloud.com>
2018-11-09 08:06:37 -0800

    first commit

--- MyMovies/MyMovies/Model Controllers/MovieController.swift
+++ MyMovies/MyMovies/Model Controllers/MovieController.swift
@@ -10,6 +10,7 @@ import Foundation
 
 class MovieController {
     
+    
     private let apiKey = "4cc920dab8b729a619647ccc4d191d5e"
     private let baseURL = URL(string: "https://api.themoviedb.org/3/search/movie")!
     
