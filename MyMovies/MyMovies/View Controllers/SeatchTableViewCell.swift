265ca863ac11b328f79c4b5cd21486d9f0ac648c
yzhukovs <yzhukovs@bumnetworks.com>
2018-11-09 14:31:55 -0500

    completed I think with weird looking button

---
+++ MyMovies/MyMovies/View Controllers/SeatchTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  SeatchTableViewCell.swift
+//  MyMovies
+//
+//  Created by Yvette Zhukovsky on 11/9/18.
+//  Copyright © 2018 Lambda School. All rights reserved.
+//
+
+import UIKit
+
+class SeatchTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
