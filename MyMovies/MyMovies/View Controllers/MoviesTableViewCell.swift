893a7f66d42f055d390eb99cb6cb15f0a13320a4
yzhukovs <yzhukovs@bumnetworks.com>
2018-11-09 13:10:08 -0500

    MyMoviesTableViewController

---
+++ MyMovies/MyMovies/View Controllers/MoviesTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  MoviesTableViewCell.swift
+//  MyMovies
+//
+//  Created by Yvette Zhukovsky on 11/9/18.
+//  Copyright © 2018 Lambda School. All rights reserved.
+//
+
+import UIKit
+
+class MoviesTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
