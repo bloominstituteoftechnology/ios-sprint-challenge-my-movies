9e134eea7100028ac2cde57e15e756e2222d7d4d
Lotanna <sugabelly@gmail.com>
2019-01-18 12:08:36 -0500

    Created Core Data Stack

---
+++ MyMovies/MyMovies/Models/MovieConvenience.swift
@@ -0,0 +1,9 @@
+//
+//  MovieConvenience.swift
+//  MyMovies
+//
+//  Created by Lotanna Igwe-Odunze on 1/18/19.
+//  Copyright © 2019 Lambda School. All rights reserved.
+//
+
+import Foundation
---
+++ MyMovies/MyMovies/Resources/CoreDataStack.swift
@@ -0,0 +1,55 @@
+//
+//  CoreDataStack.swift
+//  MyMovies
+//
+//  Created by Lotanna Igwe-Odunze on 1/18/19.
+//  Copyright © 2019 Lambda School. All rights reserved.
+//
+
+import Foundation
+import CoreData
+
+class CoreDataStack {
+    
+    static let shared = CoreDataStack()
+    let container: NSPersistentContainer
+    let mainContext: NSManagedObjectContext
+    
+    private init() {
+        container = NSPersistentContainer(name: "Movies")
+        
+        //Loads movie data from the Movies data object
+        container.loadPersistentStores { (description, error) in
+            if let loadError = error { fatalError("Couldn't load the data store: \(loadError)") }
+        } //End of Load
+        
+        //Merge any changes
+        container.viewContext.automaticallyMergesChangesFromParent = true
+
+        //Initialise the Main Context
+        mainContext = container.viewContext
+
+    } // End of Init
+    
+    //Save after merging changes
+    static func saveAfterMerging(moc: NSManagedObjectContext) throws {
+        
+        var theError: Error? //Allow for an error
+        
+        //moc.performAndWait {
+            
+            do {
+                try moc.save() //Attempt to save
+                
+            } catch {
+                
+                theError = error //If there's an error, assign it for use
+            }
+        //} End of Perform and Wait
+        
+        if let SAMerror = theError { //Unwrap the error
+            throw SAMerror //Report the error
+        }
+    }
+
+}//End of CoreData Stack
