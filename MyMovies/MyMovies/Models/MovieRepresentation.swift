8c0b2780660e5a4d73ecc8ded2c9e248152c5bd9
jkaunert <44586402+jkaunert@users.noreply.github.com>
2019-01-25 11:22:38 -0600

    implements equatable methods to MovieRepresentation.swift necessary for equality operations between Movie and MovieRepresentation

--- MyMovies/MyMovies/Models/MovieRepresentation.swift
+++ MyMovies/MyMovies/Models/MovieRepresentation.swift
@@ -27,3 +27,23 @@ struct MovieRepresentation: Equatable, Codable {
 struct MovieRepresentations: Codable {
     let results: [MovieRepresentation]
 }
+
+//MARK: - Equatable methods
+
+// these are necessary for updatePersistentStore binary operations to work
+func == (lhs: MovieRepresentation, rhs: Movie) -> Bool {
+    return lhs.identifier == rhs.identifier && lhs.title == rhs.title && lhs.hasWatched == rhs.hasWatched
+}
+
+func == (lhs: Movie, rhs: MovieRepresentation) -> Bool {
+    return rhs == lhs
+}
+
+func != (lhs: MovieRepresentation, rhs: Movie) -> Bool {
+    return !(rhs == lhs)
+}
+
+func != (lhs: Movie, rhs: MovieRepresentation) -> Bool {
+    return rhs != lhs
+}
+
