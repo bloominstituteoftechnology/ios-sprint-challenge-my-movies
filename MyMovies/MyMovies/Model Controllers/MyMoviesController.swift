c733423e7a746a4bb705100df4b7cfdcfe907f8b
jkaunert <44586402+jkaunert@users.noreply.github.com>
2019-01-25 10:57:56 -0600

    implements CoreDataStack class

--- MyMovies/MyMovies/Model Controllers/MyMoviesController.swift
+++ MyMovies/MyMovies/Model Controllers/MyMoviesController.swift
@@ -2,7 +2,7 @@
 //  MyMoviesController.swift
 //  MyMovies
 //
-//  Created by TuneUp Shop  on 1/25/19.
+//  Created by jkaunert on 1/25/19.
 //  Copyright © 2019 Lambda School. All rights reserved.
 //
 
