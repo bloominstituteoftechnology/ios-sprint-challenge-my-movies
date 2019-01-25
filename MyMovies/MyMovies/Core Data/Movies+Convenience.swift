11ad21200afa3b8d41e3e19ae23f70f17be155ae
jkaunert <44586402+jkaunert@users.noreply.github.com>
2019-01-25 11:09:51 -0600

    implements extension on Movie, adding convenience initializers for Movie and MovieRepresentation

--- MyMovies/MyMovies/Core Data/Movies+Convenience.swift
+++ MyMovies/MyMovies/Core Data/Movies+Convenience.swift
@@ -7,3 +7,45 @@
 //
 
 import Foundation
+import CoreData
+
+extension Movie {
+    
+    convenience init(identifier: UUID = UUID(),
+                     title: String,
+                     hasWatched: Bool = false,
+                     managedObjectContext: NSManagedObjectContext = CoreDataStack.shared.mainContext) {
+        self.init(context: managedObjectContext)
+        self.identifier = identifier
+        self.title = title
+        self.hasWatched = hasWatched
+        
+    }
+    
+    // Movie from Representation
+    convenience init(representation: MovieRepresentation, managedObjectContext: NSManagedObjectContext = CoreDataStack.shared.mainContext) {
+        
+        //calls movie convenience init()
+        self.init(identifier: representation.identifier ?? UUID(),
+                  title: representation.title,
+                  hasWatched: representation.hasWatched ?? false,
+                  managedObjectContext: managedObjectContext)
+        
+    }
+    
+    // Representation from Movie
+    
+    var movieRepresentation: MovieRepresentation? {
+        guard let title = title else { return nil }
+        
+        var movieID: UUID! = identifier
+        if identifier == nil {
+            movieID = UUID()
+            identifier = movieID
+        }
+        //return new representation
+        return MovieRepresentation(title: title, identifier: movieID,
+                                   hasWatched: hasWatched)
+    }
+    
+}
