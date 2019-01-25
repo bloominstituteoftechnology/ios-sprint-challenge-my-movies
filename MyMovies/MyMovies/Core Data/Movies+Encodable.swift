a1b1bb6d28b564e8fb92f6f916741be94b300edc
jkaunert <44586402+jkaunert@users.noreply.github.com>
2019-01-25 11:17:36 -0600

    implements extension on Movie, adds CodingKeysand conforms to Encodable

--- MyMovies/MyMovies/Core Data/Movies+Encodable.swift
+++ MyMovies/MyMovies/Core Data/Movies+Encodable.swift
@@ -7,3 +7,22 @@
 //
 
 import Foundation
+import CoreData
+
+extension Movie: Encodable {
+    enum CodingKeys: String, CodingKey {
+        case title
+        case identifier
+        case hasWatched
+    }
+    
+    public func encode(to encoder: Encoder) throws {
+        
+        var container = encoder.container(keyedBy: CodingKeys.self)
+        
+        try container.encode(self.title, forKey: .title)
+        try container.encode(self.identifier, forKey: .identifier)
+        try container.encode(self.hasWatched, forKey: .hasWatched)
+    }
+    
+}
