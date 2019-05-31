ca312f436d9688aa6a9d15946dcb2b0246b625cc
victor <ruizdvictor@gmail.com>
2019-05-31 12:36:02 -0400

    added my movie cell logic

---
+++ MyMovies/MyMovies/View Controllers/MovieTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  MovieTableViewCell.swift
+//  MyMovies
+//
+//  Created by Victor  on 5/31/19.
+//  Copyright © 2019 Lambda School. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class MovieTableViewCell: UITableViewCell, MovieProtocol {
+    
+    var movieController: MovieController?
+    
+    
+    
+    
+    
+    
+    
+    
+}
