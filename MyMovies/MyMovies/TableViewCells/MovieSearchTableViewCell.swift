95951db9a6149787fda7d1bf9444e4418d0a3ed6
hectorsvill <hectorstevenvillasano@gmail.com>
2019-05-31 08:11:20 -0700

    MovieSearchTableViewCell

---
+++ MyMovies/MyMovies/TableViewCells/MovieSearchTableViewCell.swift
@@ -0,0 +1,19 @@
+//
+//  MovieSearchTableViewCell.swift
+//  MyMovies
+//
+//  Created by Hector Steven on 5/31/19.
+//  Copyright © 2019 Lambda School. All rights reserved.
+//
+
+import UIKit
+
+class MovieSearchTableViewCell: UITableViewCell {
+
+	@IBAction func AddMovieButton(_ sender: Any) {
+		
+	}
+	
+	@IBOutlet var titleLable: UILabel!
+
+}
