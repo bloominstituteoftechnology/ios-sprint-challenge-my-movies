739efa2560f2f37c2232fb6e1deeddcd537b9c78
victor <ruizdvictor@gmail.com>
2019-05-31 13:43:47 -0400

    updated search table view controller delegate

--- MyMovies/MyMovies/View Controllers/MyMoviesTableViewController.swift
+++ MyMovies/MyMovies/View Controllers/MyMoviesTableViewController.swift
@@ -19,18 +19,25 @@ class MyMoviesTableViewController: UITableViewController {
         // Dispose of any resources that can be recreated.
     }
 
-    // MARK: - Table view data source
-
-    override func numberOfSections(in tableView: UITableView) -> Int {
-        // #warning Incomplete implementation, return the number of sections
-        return 0
-    }
-
-    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        // #warning Incomplete implementation, return the number of rows
-        return 0
-    }
-
-    
-
 }
+
+extension MyMoviesTableViewController {
+    
+    
+    
+}
+
+extension MyMoviesTableViewController {
+    
+    
+    
+}
+
+extension MyMoviesTableViewController {
+    
+    
+    
+}
+
+
+
