c00cec1e3c519aafae10281fd49cbfe09b70ea4c
hectorsvill <hectorstevenvillasano@gmail.com>
2019-05-31 08:40:14 -0700

    CoreDataStack class

--- MyMovies/MyMovies/Models/CoreDataStack.swift
+++ MyMovies/MyMovies/Models/CoreDataStack.swift
@@ -12,6 +12,36 @@ import CoreData
 class CoreDataStack {
 	static let shared = CoreDataStack()
 	
+	func save(context: NSManagedObjectContext) throws {
+		var error: Error?
+		
+		context.performAndWait {
+			do {
+				try context.save()
+			} catch let saveError {
+				NSLog("Error saving moc: \(saveError)")
+				error = saveError
+			}
+		}
+		
+		if let error = error {
+			throw error
+		}
+	}
 	
+	lazy var container: NSPersistentContainer = {
+		let container = NSPersistentContainer(name: "Movie")
+		
+		container.loadPersistentStores { _, error in
+			if let error = error {
+				fatalError("Failed to load presistent store: \(error)")
+			}
+		}
 	
+		return container
+	}()
+	
+	var mainContext: NSManagedObjectContext {
+		return container.viewContext
+	}
 }
