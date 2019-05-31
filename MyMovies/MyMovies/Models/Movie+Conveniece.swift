8c64dbf1d604eba0a04561a6fbcb9a63d8f842d5
hectorsvill <hectorstevenvillasano@gmail.com>
2019-05-31 08:51:56 -0700

    convenience init (title, identifier, hasWatched, context

--- MyMovies/MyMovies/Models/Movie+Conveniece.swift
+++ MyMovies/MyMovies/Models/Movie+Conveniece.swift
@@ -7,9 +7,16 @@
 //
 
 import Foundation
+import CoreData
 extension Movie {
 	
-	
+	convenience init (title: String, identifier: UUID = UUID(), hasWatched: Bool = false,
+					  context: NSManagedObjectContext = CoreDataStack.shared.mainContext) {
+		self.init(context: context)
+		self.title = title
+		self.identifier = identifier
+		self.hasWatched = hasWatched
+	}
 	
 	
 	
