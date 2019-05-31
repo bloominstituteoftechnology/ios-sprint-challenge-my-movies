a768ed7bda4e194f4df843220178881fe1ee4ea9
victor <ruizdvictor@gmail.com>
2019-05-31 12:39:24 -0400

    added save movie functionality to movie table view cell

--- MyMovies/MyMovies/View Controllers/MovieTableViewCell.swift
+++ MyMovies/MyMovies/View Controllers/MovieTableViewCell.swift
@@ -12,12 +12,21 @@ import UIKit
 class MovieTableViewCell: UITableViewCell, MovieProtocol {
     
     var movieController: MovieController?
+    @IBOutlet weak var movieLabel: UILabel!
     
+    var movieRepresentation: MovieRepresentation? {
+        didSet {
+            updateViews()
+        }
+    }
     
+    private func updateViews(){
+        guard let movieRepresentation = movieRepresentation else {return}
+        movieLabel.text = movieRepresentation.title
+    }
     
-    
-    
-    
-    
-    
+    @IBAction func saveMovie(_ sender: Any) {
+        guard let movie = movieRepresentation else { return }
+        movieController?.createMovie(title: movie.title)
+    }
 }
