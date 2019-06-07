96d0ad13f623cc32330a3d86e750710e1c592f07
Ryan Murphy <thomasryanmurphy@gmail.com>
2019-06-07 11:11:24 -0400

    Built custom Cells

---
+++ MyMovies/MyMovies/View Controllers/MyMovieTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  MyMovieTableViewCell.swift
+//  MyMovies
+//
+//  Created by Ryan Murphy on 6/7/19.
+//  Copyright © 2019 Lambda School. All rights reserved.
+//
+
+import UIKit
+
+class MyMovieTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
