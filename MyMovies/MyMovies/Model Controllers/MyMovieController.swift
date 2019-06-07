79b67c80caf377d16f0e25107a8fb229e741c3d8
Michael R.F <mrflowers22@uwalumni.com>
2019-06-07 12:09:08 -0400

    implemented all the functions for myMovieController file

--- MyMovies/MyMovies/Model Controllers/MyMovieController.swift
+++ MyMovies/MyMovies/Model Controllers/MyMovieController.swift
@@ -17,7 +17,8 @@ class MyMovieController {
     //MARK: Core Data CRUD functions
     
     func createMovie(title: String){
-        _ = Movie(title: title)
+        let newMovie = Movie(title: title)
+        put(movie: newMovie)
         saveToPersistentStore()
     }
     
@@ -28,6 +29,7 @@ class MyMovieController {
     
     func toggle(movie: Movie){
         movie.hasWatched.toggle()
+        put(movie: movie)
         saveToPersistentStore()
     }
     
@@ -62,6 +64,85 @@ class MyMovieController {
             }
             completion(nil)
         }.resume()
+    }
+    
+    func deleteFromServer(movie: Movie, completion: @escaping completionHandler = {_ in }){
+        guard let identifier = movie.identifier?.uuidString else { print("Error unwrapping identifier"); completion(NSError()); return }
+        let url = baseURL.appendingPathComponent(identifier).appendingPathExtension("json")
+        var request = URLRequest(url: url)
+        request.httpMethod = "DELETE"
+     
+        URLSession.shared.dataTask(with: request) { (_, response, error) in
+            if let response = response as? HTTPURLResponse {
+                print("This is the statusCode for deleting a movieRep off of the server: \(response.statusCode)")
+            }
+            if let error = error {
+                print("Error deleting off the server: \(error.localizedDescription)")
+                completion(error)
+                return
+            }
+            completion(nil)
+            }.resume()
+    }
+    
+    func fetchFromServer(completion: @escaping completionHandler = {_ in }){
+        let url = baseURL.appendingPathExtension("json")
+        
+        URLSession.shared.dataTask(with: url) { (data, response, error) in
+            if let response = response as? HTTPURLResponse {
+                print("This is the statusCode for fetching all movieReps from the server: \(response.statusCode)")
+            }
+            if let error = error {
+                print("Error fetching from the server: \(error.localizedDescription)")
+                completion(error)
+                return
+            }
+            guard let data = data else { print("Error unwrapping data fetching from server"); completion(NSError()); return }
+            
+            let jD = JSONDecoder()
+            
+            do {
+                let movieRepDict = try jD.decode([ String : MovieRepresentation ].self, from: data)
+                let movieRepArray = Array(movieRepDict.values)
+                
+                //loop through the array of movieReps
+                for movieRep in movieRepArray {
+                    //check to see if the movieRep is already saved in core data
+                    if let movieInCoreData = self.checkMovieInCoreData(with: movieRep.title){
+                        //movie is in core data and on server just update the movie in core data
+                        self.update(movie: movieInCoreData, withMovieRep: movieRep)
+                    } else {
+                        //we have a movieRep on server but not in core data so we have to initialize a movie in core data with the values of the movieRep
+                        _ = Movie(movieRepresentation: movieRep)
+                    }
+                }
+                 self.saveToPersistentStore()
+            } catch {
+                print("Error decoding from server: \(error.localizedDescription)")
+                completion(error)
+                return
+            }
+        }
+    }
+    
+    func update(movie: Movie, withMovieRep: MovieRepresentation){
+        movie.title = withMovieRep.title
+        movie.hasWatched = withMovieRep.hasWatched!
+    }
+    
+    func checkMovieInCoreData(with title: String) -> Movie? {
+        let fetchRequest: NSFetchRequest<Movie> = Movie.fetchRequest()
+        fetchRequest.predicate = NSPredicate(format: "title == %@", title)
+        let moc = CoreDataStack.shared.mainContext
+        var movie: Movie? = nil
+        do {
+            //remember you want one, so we have to use the first property
+           movie = try moc.fetch(fetchRequest).first
+        } catch  {
+            print("Error checking movie in core data with title of movieRep: \(error.localizedDescription)")
+            movie = nil
+        }
+        return movie
     }
     
     func saveToPersistentStore(){
