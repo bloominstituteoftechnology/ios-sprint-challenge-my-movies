a83a96542af415dbd3eb71071bb577dfcde6a6f6
Kobe McKee <kobe.mckee@gmail.com>
2019-06-07 10:16:05 -0600

    Fleshed out MovieController with server and local functions

--- MyMovies/MyMovies/Model Controllers/MovieController.swift
+++ MyMovies/MyMovies/Model Controllers/MovieController.swift
@@ -7,8 +7,22 @@
 //
 
 import Foundation
+import CoreData
 
 class MovieController {
+    
+    
+    //init() { fetchMoviesFromServer() }
+    
+    
+    // MARK: - Properties
+    
+    var searchedMovies: [MovieRepresentation] = []
+typealias CompletionHandler = (Error?) -> Void
+    
+    
+    
+    // MARK: - API Search
     
     private let apiKey = "4cc920dab8b729a619647ccc4d191d5e"
     private let baseURL = URL(string: "https://api.themoviedb.org/3/search/movie")!
@@ -52,7 +66,167 @@ class MovieController {
         }.resume()
     }
     
-    // MARK: - Properties
     
-    var searchedMovies: [MovieRepresentation] = []
+    
+    
+    // MARK: - CRUD
+    
+    func createMovie(title: String, identifier: UUID, hasWatched: Bool) {
+        let movie = Movie(title: title, identifier: identifier, hasWatched: hasWatched)
+        put(movie: movie)
+    }
+    
+    func toggleWatched(movie: Movie, hasWatched: Bool) {
+        movie.hasWatched = !movie.hasWatched
+        put(movie: movie)
+    }
+    
+    
+    
+    // MARK: - Server Functions
+    
+    let serverURL = URL(string: "https://mymovies-19800.firebaseio.com/")!
+    
+    
+    
+    func put(movie: Movie, completion: @escaping CompletionHandler = {_ in}) {
+        
+        let uuid = movie.identifier ?? UUID()
+        
+        let requestURL = serverURL
+            .appendingPathComponent(uuid.uuidString)
+            .appendingPathExtension("json")
+        
+        
+        var request = URLRequest(url: requestURL)
+        request.httpMethod = "PUT"
+        
+        
+        do {
+            guard let representation = movie.movieRepresentation else { throw NSError() }
+            try CoreDataStack.shared.save()
+            request.httpBody = try JSONEncoder().encode(representation)
+        } catch {
+            NSLog("Error encoding movie: \(error)")
+            completion(error)
+            return
+        }
+        
+        
+        URLSession.shared.dataTask(with: request) { (_, _, error) in
+            if let error = error {
+                NSLog("Error PUTTING movie to server: \(error)")
+                completion(error)
+                return
+            }
+            completion(nil)
+        }.resume()
+    }
+    
+    
+    
+    func deleteMovieFromServer(movie: Movie, completion: @escaping CompletionHandler = {_ in}) {
+        
+        let uuid = movie.identifier ?? UUID()
+        
+        let requestURL = serverURL
+            .appendingPathComponent(uuid.uuidString)
+            .appendingPathExtension("json")
+        
+        var request = URLRequest(url: requestURL)
+        request.httpMethod = "DELETE"
+        
+        URLSession.shared.dataTask(with: request) { (_, _, error) in
+            if let error = error {
+                NSLog("Error deleting movie: \(error)")
+                completion(error)
+                return
+            }
+            completion(nil)
+        }.resume()
+    }
+    
+    
+    
+    func fetchMoviesFromSever(completion: @escaping CompletionHandler = {_ in}) {
+        
+        let requestURL = serverURL.appendingPathExtension("json")
+        
+        URLSession.shared.dataTask(with: requestURL) { (data, _, error) in
+            if let error = error {
+                NSLog("Error fetching movies: \(error)")
+                completion(error)
+                return
+            }
+            
+            guard let data = data else {
+                NSLog("No data returned by data task")
+                completion(NSError())
+                return
+            }
+            
+            
+            do {
+                let movieRepresentations = Array(try JSONDecoder().decode([String : MovieRepresentation].self, from: data).values)
+                let moc = CoreDataStack.shared.container.newBackgroundContext()
+                try self.updateMovies(representations: movieRepresentations, context: moc)
+                completion(nil)
+            } catch {
+                NSLog("Error decoding movie representations: \(error)")
+                completion(error)
+                return
+            }
+        }.resume()
+    }
+    
+    
+    func updateMovies(representations: [MovieRepresentation], context: NSManagedObjectContext) throws {
+        
+        var error: Error? = nil
+        context.performAndWait {
+            for movieRep in representations {
+                guard let uuid = UUID(uuidString: movieRep.identifier!.uuidString) else { continue }
+                
+                if let movie = self.fetchSingleMovieFromPersistentStore(identifier: uuid.uuidString, context: context) {
+                    self.update(movie: movie, movieRep: movieRep)
+                } else {
+                    let _ = Movie(movieRepresentation: movieRep)
+                }
+            }
+            
+            
+            do {
+                try context.save()
+            } catch let saveError {
+                error = saveError
+            }
+        }
+        if let error = error { throw error }
+    }
+    
+    
+    
+    func update(movie: Movie, movieRep: MovieRepresentation) {
+        movie.title = movieRep.title
+        movie.identifier = movieRep.identifier
+        movie.hasWatched = movieRep.hasWatched!
+    }
+    
+    
+    
+    func fetchSingleMovieFromPersistentStore(identifier: String, context: NSManagedObjectContext) -> Movie? {
+        
+        let fetchRequest: NSFetchRequest<Movie> = Movie.fetchRequest()
+        fetchRequest.predicate = NSPredicate(format: "identifier == %@", identifier)
+        
+        var result: Movie? = nil
+        context.performAndWait {
+            do {
+                result = try context.fetch(fetchRequest).first
+            } catch {
+                NSLog("Error fetching movie with uuid: \(identifier): \(error)")
+            }
+        }
+        return result
+    }
 }
