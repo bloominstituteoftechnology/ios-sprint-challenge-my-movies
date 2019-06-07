6bcf638d09789bfb7f96fdf7c79770c0d8b59c2b
Lisa Sampson <lisasampson0918@gmail.com>
2019-06-07 11:41:26 -0400

    Set up model

---
+++ MyMovies/MyMovies/Models/Movie+Convenience.swift
@@ -0,0 +1,27 @@
+//
+//  Movie+Convenience.swift
+//  MyMovies
+//
+//  Created by Lisa Sampson on 6/7/19.
+//  Copyright © 2019 Lambda School. All rights reserved.
+//
+
+import Foundation
+import CoreData
+
+extension Movie {
+    convenience init(title: String, hasWatched: Bool = false, identifier: UUID = UUID(), context: NSManagedObjectContext = CoreDataStack.shared.mainContext) {
+        self.init(context: context)
+        self.title = title
+        self.hasWatched = hasWatched
+        self.identifier = identifier
+    }
+    
+    convenience init(movieRepresentation: MovieRepresentation, context: NSManagedObjectContext = CoreDataStack.shared.mainContext) {
+        let title = movieRepresentation.title
+        let hasWatched = movieRepresentation.hasWatched ?? false
+        let identifier = movieRepresentation.identifier ?? UUID()
+        
+        self.init(title: title, hasWatched: hasWatched, identifier: identifier, context: context)
+    }
+}
