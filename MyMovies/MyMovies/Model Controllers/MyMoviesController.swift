ab14e135ded305273c21c61e4469e41b197fe339
Sameera Leola <pixelandbyte@gmail.com>
2019-06-07 10:38:49 -0500

    Updated movie search cell with Add Movie button

---
+++ MyMovies/MyMovies/Model Controllers/MyMoviesController.swift
@@ -0,0 +1,16 @@
+//
+//  MyMoviesController.swift
+//  MyMovies
+//
+//  Created by Sameera Roussi on 6/7/19.
+//  Copyright © 2019 Lambda School. All rights reserved.
+//
+
+import Foundation
+import CoreData
+
+let baseURL = URL(string: "https://mymovies-20257.firebaseio.com/")
+
+class MyMoviesController {
+    
+}
---
+++ MyMovies/MyMovies/View Controllers/MovieListTableViewCell.swift
@@ -0,0 +1,17 @@
+//
+//  MovieListTableViewCell.swift
+//  MyMovies
+//
+//  Created by Sameera Roussi on 6/7/19.
+//  Copyright © 2019 Lambda School. All rights reserved.
+//
+
+import UIKit
+
+class MovieListTableViewCell: UITableViewCell {
+
+    // MARK: - Actions
+    @IBAction func addMovieButtonTapped(_ sender: Any) {
+        print("Add movie button tapped.  The movie name is \(textLabel?.text)")
+    }
+}
