8506c413c9ce986a996554ac2a78f0886ab5c935
Thomas Cacciatore <tcacciatore597@gmail.com>
2019-06-14 12:04:26 -0400

    created Core Data Stack and movie convenience initializers. Created move model in core data. Added a button to the movie search table view cell.

--- MyMovies/MyMovies/View Controllers/MovieSearchTableViewController.swift
+++ MyMovies/MyMovies/View Controllers/MovieSearchTableViewController.swift
@@ -16,6 +16,10 @@ class MovieSearchTableViewController: UITableViewController, UISearchBarDelegate
         searchBar.delegate = self
     }
     
+    @IBAction func addMovieButtonTapped(_ sender: Any) {
+    }
+    
+    
     func searchBarSearchButtonClicked(_ searchBar: UISearchBar) {
         guard let searchTerm = searchBar.text else { return }
         
