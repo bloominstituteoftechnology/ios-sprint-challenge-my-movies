abbae20e754c9066367a487fcae24a45e60ea819
Seschwan <seschwan@Seschwans-MacBook-Pro.local>
2019-07-19 22:18:27 -0600

    Added Put Method

--- MyMovies/MyMovies/Model Controllers/MovieController.swift
+++ MyMovies/MyMovies/Model Controllers/MovieController.swift
@@ -8,11 +8,18 @@
 
 import Foundation
 
+enum HTTPMethods: String {
+    case get    = "GET"
+    case post   = "POST"
+    case put    = "PUT"
+    case delete = "DELETE"
+}
+
 class MovieController {
     
     // MARK: - FireBase URL
     
-    let fireBaseURL = URL(string: "https://mymoviessprintchallenge.firebaseio.com/")
+    let fireBaseURL = URL(string: "https://mymoviessprintchallenge.firebaseio.com/")!
     
     // MARK: - API
     
@@ -65,6 +72,41 @@ class MovieController {
 
 extension MovieController {
     
+    
+    // MARK: - Firebase
+    
+    typealias CompletionHandler = (Error?) -> Void
+    
+    func put(movie: Movie, completion: @escaping CompletionHandler = { _ in }) {
+        let uuid = movie.identifier ?? UUID()
+        let requestURL = fireBaseURL.appendingPathComponent(uuid.uuidString).appendingPathExtension("json")
+        var request = URLRequest(url: requestURL)
+        request.httpMethod = HTTPMethods.put.rawValue
+        
+        do {
+            guard let representation = movie.movieReprensentation else { // Should it be a `var`?
+                completion(NSError())
+                return
+            }
+            //representation.identifier = uuid.uuidString // is this needed?
+            movie.identifier = uuid
+            // try saveToPersistentStore()
+            request.httpBody = try JSONEncoder().encode(representation)
+        } catch {
+            NSLog("Error ecoding movie: \(movie) \(error)")
+            completion(error)
+            return
+        }
+        URLSession.shared.dataTask(with: request) { (data, _, error) in
+            if let error = error {
+                NSLog("Error PUTting to the server")
+                completion(error)
+                return
+            }
+            completion(nil)
+        }.resume()
+        
+    }
     
     
 }
