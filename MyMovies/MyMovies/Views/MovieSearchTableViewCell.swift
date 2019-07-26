ad0efe6435216402ac43efc54cfe1d62b23ad5c8
Kat Milton <mltnkat@gmail.com>
2019-07-26 10:08:11 -0700

    Adding movies to database successful.

---
+++ MyMovies/MyMovies/Views/MovieSearchTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  MovieSearchTableViewCell.swift
+//  MyMovies
+//
+//  Created by Kat Milton on 7/26/19.
+//  Copyright © 2019 Lambda School. All rights reserved.
+//
+
+import UIKit
+
+class MovieSearchTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
---
+++ MyMovies/MyMovies/Views/MoviesTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  MoviesTableViewCell.swift
+//  MyMovies
+//
+//  Created by Kat Milton on 7/26/19.
+//  Copyright © 2019 Lambda School. All rights reserved.
+//
+
+import UIKit
+
+class MoviesTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
