2e1420ce3c2c4075375be3295e005686f732c087
seanacres <6416690+seanacres@users.noreply.github.com>
2019-07-26 12:05:14 -0400

    Set up my movie cell and update hasWatched

--- MyMovies/MyMovies/Views/MyMoviesTableViewCell.swift
+++ MyMovies/MyMovies/Views/MyMoviesTableViewCell.swift
@@ -10,12 +10,36 @@ import UIKit
 
 class MyMoviesTableViewCell: UITableViewCell {
 
-    var movie: Movie?
+    var movie: Movie? {
+        didSet {
+            updateViews()
+        }
+    }
+    
     var movieController: MovieController?
     
     @IBOutlet weak var titleLabel: UILabel!
-
+    @IBOutlet weak var hasWatchedButton: UIButton!
+    
     @IBAction func hasWatchedTapped(_ sender: Any) {
+        guard let movie = movie,
+            let movieController = movieController else { return }
+        
+        movie.hasWatched.toggle()
+        movieController.updateMovie(movie: movie, hasWatched: movie.hasWatched)
+        
+        updateViews()
+    }
+    
+    func updateViews() {
+        guard let movie = movie else { return }
+        
+        titleLabel.text = movie.title
+        if movie.hasWatched {
+            hasWatchedButton.setTitle("Watched", for: .normal)
+        } else {
+            hasWatchedButton.setTitle("Unwatched", for: .normal)
+        }
         
     }
 }
