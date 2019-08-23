cfe12974eb0557addda7d5b552d73b0a5803ee8c
Marlon <marmoneymar@icloud.com>
2019-08-23 16:42:14 -0400

    UIChanges. addMovie button change color to alert user movie's been added

---
+++ MyMovies/MyMovies/Helpers/Colors.swift
@@ -0,0 +1,14 @@
+//
+//  Colors.swift
+//  MyMovies
+//
+//  Created by Marlon Raskin on 8/23/19.
+//  Copyright © 2019 Lambda School. All rights reserved.
+//
+
+import UIKit
+
+extension UIColor {
+	static let movieDBGreen = UIColor(red: 0.00, green: 0.82, blue: 0.47, alpha: 1.00)
+	static let movieDBDarkBlue = UIColor(red: 0.03, green: 0.11, blue: 0.14, alpha: 1.00)
+}
