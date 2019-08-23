4ad38526410895a2259376100e1899df2c94be77
Jeffrey Santana <SantanaJny@gmail.com>
2019-08-23 11:50:33 -0400

    Initial commit

--- MyMovies/MyMovies/View Controllers/MyMoviesTableVC.swift
+++ MyMovies/MyMovies/View Controllers/MyMoviesTableVC.swift
@@ -1,5 +1,5 @@
 //
-//  MyMoviesTableViewController.swift
+//  MyMoviesTableVC.swift
 //  MyMovies
 //
 //  Created by Spencer Curtis on 8/17/18.
@@ -8,22 +8,27 @@
 
 import UIKit
 
-class MyMoviesTableViewController: UITableViewController {
+class MyMoviesTableVC: UITableViewController {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-
-        // Uncomment the following line to preserve selection between presentations
-        // self.clearsSelectionOnViewWillAppear = false
-
-        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
-        // self.navigationItem.rightBarButtonItem = self.editButtonItem
-    }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
+	//MARK: - IBOutlets
+	
+	
+	//MARK: - Properties
+	
+	
+	//MARK: - Life Cycle
+	
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		
+		
+	}
+	
+	//MARK: - IBActions
+	
+	
+	//MARK: - Helpers
+	
 
     // MARK: - Table view data source
 
