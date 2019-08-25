19e7741a6b0ce6e737297e09e2f304dc850c9196
Nate Hedgeman <nate.hedgeman@gmail.com>
2019-08-25 18:08:49 -0400

    added search cell delegate to add movie to core data.

---
+++ MyMovies/View Controllers/SearchMovieTableViewCell.swift
@@ -0,0 +1,18 @@
+//
+//  SearchMovieTableViewCell.swift
+//  MyMovies
+//
+//  Created by Nathan Hedgeman on 8/25/19.
+//  Copyright © 2019 Lambda School. All rights reserved.
+//
+
+import UIKit
+
+class SearchMovieTableViewCell: UITableViewCell {
+    //Properties
+    @IBOutlet weak var titleLabel: UILabel!
+    var delegate: SearchMovieTableViewCellDelegate?
+    @IBAction func addMovieToCoreDataButtonTapped(_ sender: Any) {
+        delegate?.addMovieToCoreData(for: self)
+    }
+}
