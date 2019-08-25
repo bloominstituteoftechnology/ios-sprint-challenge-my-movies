58273fe8b053af5ded49cd0230791ab98ed846e4
Nate Hedgeman <nate.hedgeman@gmail.com>
2019-08-25 17:14:32 -0400

    added core data stack, convenience init and data model.

--- MyMovies/Model Controllers/MovieController.swift
+++ MyMovies/Model Controllers/MovieController.swift
@@ -56,3 +56,5 @@ class MovieController {
     
     var searchedMovies: [MovieRepresentation] = []
 }
+
+
---
+++ MyMovies/Models/Movie+Convienience.swift
@@ -0,0 +1,36 @@
+//
+//  Movie+Convienience.swift
+//  MyMovies
+//
+//  Created by Nathan Hedgeman on 8/25/19.
+//  Copyright © 2019 Lambda School. All rights reserved.
+//
+
+import Foundation
+import CoreData
+
+extension Movie {
+    var movieRepresentation: MovieRepresentation? {
+        
+        guard let title = self.title else { return nil }
+        
+        return MovieRepresentation(title: title, identifier: identifier, hasWatched: hasWatched)
+    }
+    
+    convenience init(title: String, identifier: UUID = UUID(), hasWatched: Bool = false, context: NSManagedObjectContext = CoreDataStack.shared.mainContext) {
+        
+        self.init(context: context)
+        
+        self.title = title
+        self.identifier = identifier
+        self.hasWatched = hasWatched
+    }
+    
+    convenience init?(movieRepresentation: MovieRepresentation, context: NSManagedObjectContext = CoreDataStack.shared.mainContext) {
+        
+        guard let identifier = movieRepresentation.identifier else { return nil }
+        guard let hasWatched = movieRepresentation.hasWatched else { return nil }
+        
+        self.init(title: movieRepresentation.title, identifier: identifier, hasWatched: hasWatched, context: context)
+    }
+}
