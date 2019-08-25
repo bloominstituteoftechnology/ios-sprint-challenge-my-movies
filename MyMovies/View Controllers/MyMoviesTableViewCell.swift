1d9b939ae205c45a28b2d2c38d52c9f38cbdac2e
Nate Hedgeman <nate.hedgeman@gmail.com>
2019-08-25 18:28:22 -0400

    added table view data source and delegate. added update views function to  custom cell. added table view cell delegate.

--- MyMovies/View Controllers/MyMoviesTableViewCell.swift
+++ MyMovies/View Controllers/MyMoviesTableViewCell.swift
@@ -13,6 +13,22 @@ class MyMoviesTableViewCell: UITableViewCell {
     @IBOutlet var myMovieTitle: UILabel!
     @IBOutlet var seenNotSeenButton: UIButton!
     var delegate: MovieTableViewCellDelegate?
+    
+    var movie: Movie? {
+        didSet {
+            updateViews()
+        }
+    }
+    
+    private func updateViews() {
+        guard let movie = self.movie else { return }
+        self.myMovieTitle.text = movie.title
+        if movie.hasWatched == true {
+            self.seenNotSeenButton.setTitle("Watched", for: .normal)
+        } else {
+            self.seenNotSeenButton.setTitle("Unwatched", for: .normal)
+        }
+    }
 
     
     // Mark: - IBActions and Methods
