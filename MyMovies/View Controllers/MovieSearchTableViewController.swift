c4d73586bbd398ffb1f9ab8595d23f5488a62642
Nate Hedgeman <nate.hedgeman@gmail.com>
2019-08-27 16:56:09 -0400

    MVP WITHOUT me being an idiot.

--- MyMovies/View Controllers/MovieSearchTableViewController.swift
+++ MyMovies/View Controllers/MovieSearchTableViewController.swift
@@ -38,9 +38,10 @@ class MovieSearchTableViewController: UITableViewController, UISearchBarDelegate
     }
     
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "MovieCell", for: indexPath)
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: "MovieCell", for: indexPath) as? SearchMovieTableViewCell else {return UITableViewCell() }
         
-        cell.textLabel?.text = movieController.searchedMovies[indexPath.row].title
+        cell.titleLabel.text = movieController.searchedMovies[indexPath.row].title
+        cell.delegate = self
         
         return cell
     }
