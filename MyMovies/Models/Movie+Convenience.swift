047bc38f5750937d1989cd9da5cf4c5be34e542e
Taylor Fahey-Lyles <hawks93187@gmail.com>
2019-09-20 08:05:24 -0700

    Set up Moviedatamodel, CoreDataStack, and added Movie+Convenience

---
+++ MyMovies/Models/Movie+Convenience.swift
@@ -0,0 +1,10 @@
+//
+//  Movie+Convenience.swift
+//  MyMovies
+//
+//  Created by Taylor Lyles on 9/20/19.
+//  Copyright © 2019 Lambda School. All rights reserved.
+//
+
+import Foundation
+import CoreData
