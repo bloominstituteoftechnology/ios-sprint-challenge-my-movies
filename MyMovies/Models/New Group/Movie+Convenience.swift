c9388cb4b7e2c8753fc60d9a48559c645f42ead3
Percy <percival.ngan@gmail.com>
2019-09-23 09:27:19 -0500

    Added CoreDataStack, Convenience file

--- MyMovies/Models/New Group/Movie+Convenience.swift
+++ MyMovies/Models/New Group/Movie+Convenience.swift
@@ -11,7 +11,7 @@ import CoreData
 
 extension Movie {
 
-	convenience init(title: String, identifier: UUID, hasWatched: Bool, context: NSManagedObjectContext) {
+	convenience init(title: String, identifier: String = UUID().uuidString, hasWatched: Bool = false, context: NSManagedObjectContext) {
 
 		self.init(context: context)
 		self.title = title
---
+++ Views/MyMoviesTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  MyMoviesTableViewCell.swift
+//  MyMovies
+//
+//  Created by Percy Ngan on 9/23/19.
+//  Copyright © 2019 Lambda School. All rights reserved.
+//
+
+import UIKit
+
+class MyMoviesTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
