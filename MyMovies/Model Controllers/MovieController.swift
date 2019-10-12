c31949494ca506c02250ec5afa313e21f5576697
John Kouris <jdkouris@gmail.com>
2019-10-12 09:25:17 -0700

    Added the put method

--- MyMovies/Model Controllers/MovieController.swift
+++ MyMovies/Model Controllers/MovieController.swift
@@ -8,10 +8,51 @@
 
 import Foundation
 
+enum HTTPMethod: String {
+    case get = "GET"
+    case put = "PUT"
+    case post = "POST"
+    case delete = "DELETE"
+}
+
 class MovieController {
     
     private let apiKey = "4cc920dab8b729a619647ccc4d191d5e"
     private let baseURL = URL(string: "https://api.themoviedb.org/3/search/movie")!
+    
+    let firebaseURL = URL(string: "https://mymovies-2d3de.firebaseio.com/")!
+    
+    func put(movie: Movie, completion: @escaping () -> Void = {}) {
+        let identifier = movie.identifier ?? UUID()
+        movie.identifier = identifier
+        
+        let requestURL = firebaseURL.appendingPathComponent(identifier.uuidString).appendingPathExtension("json")
+        var request = URLRequest(url: requestURL)
+        request.httpMethod = HTTPMethod.put.rawValue
+        
+        guard let movieRepresentation = movie.movieRepresentation else {
+            print("Movie representation is nil")
+            completion()
+            return
+        }
+        
+        do {
+            request.httpBody = try JSONEncoder().encode(movieRepresentation)
+        } catch {
+            print("Error encoding movie representation: \(error)")
+            completion()
+            return
+        }
+        
+        URLSession.shared.dataTask(with: request) { (_, _, error) in
+            if let error = error {
+                print("Error PUTing movie: \(error)")
+                completion()
+                return
+            }
+            completion()
+        }.resume()
+    }
     
     func searchForMovie(with searchTerm: String, completion: @escaping (Error?) -> Void) {
         
