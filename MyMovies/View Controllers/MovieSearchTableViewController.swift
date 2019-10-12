efd7f69a85adab73ea44b8ce0e80c6f3c9477369
dillonp23 <dillonp.lambda@gmail.com>
2019-10-12 09:56:35 -0700

    Move FRC and delegate extension to MyMoviesTableView (incorrectly added to SearchTableView)

--- MyMovies/View Controllers/MovieSearchTableViewController.swift
+++ MyMovies/View Controllers/MovieSearchTableViewController.swift
@@ -11,21 +11,6 @@ import CoreData
 
 class MovieSearchTableViewController: UITableViewController, UISearchBarDelegate {
     
-    
-    lazy var fetchResultsController: NSFetchedResultsController<Movie> = {
-        let fetchRequest: NSFetchRequest<Movie> = Movie.fetchRequest()
-        fetchRequest.sortDescriptors = [
-            NSSortDescriptor(key: "hasWatched", ascending: true)
-        ]
-        let moc = CoreDataStack.shared.mainContext
-        let frc = NSFetchedResultsController(fetchRequest: fetchRequest, managedObjectContext: moc, sectionNameKeyPath: "hasWatched", cacheName: nil)
-        frc.delegate = self
-        try! frc.performFetch()
-        return frc
-        
-    }()
-    
-
     override func viewDidLoad() {
         super.viewDidLoad()
         
@@ -69,47 +54,3 @@ class MovieSearchTableViewController: UITableViewController, UISearchBarDelegate
 }
 
 
-extension MovieSearchTableViewController: NSFetchedResultsControllerDelegate {
-    
-    func controllerWillChangeContent(_ controller: NSFetchedResultsController<NSFetchRequestResult>) {
-        tableView.beginUpdates()
-    }
-    
-    func controllerDidChangeContent(_ controller: NSFetchedResultsController<NSFetchRequestResult>) {
-        tableView.endUpdates()
-    }
-    
-    func controller(_ controller: NSFetchedResultsController<NSFetchRequestResult>, didChange sectionInfo: NSFetchedResultsSectionInfo, atSectionIndex sectionIndex: Int, for type: NSFetchedResultsChangeType) {
-            
-            switch type {
-            case .insert:
-                tableView.insertSections(IndexSet(integer: sectionIndex), with: .automatic)
-            case .delete:
-                tableView.deleteSections(IndexSet(integer: sectionIndex), with: .automatic)
-            default:
-                break /// break == we're aware we're not handling stuff and intentionally doing that
-            }
-        }
-        
-        func controller(_ controller: NSFetchedResultsController<NSFetchRequestResult>, didChange anObject: Any, at indexPath: IndexPath?, for type: NSFetchedResultsChangeType, newIndexPath: IndexPath?) {
-            switch type {
-            case .insert:
-                guard let newIndexPath = newIndexPath else { return }
-                tableView.insertRows(at: [newIndexPath], with: .automatic)
-            case .update:
-                guard let indexPath = indexPath else { return }
-                tableView.reloadRows(at: [indexPath], with: .automatic)
-            case .move:
-                guard let oldIndexPath = indexPath, let newIndexPath = newIndexPath else { return }
-    //            tableView.deleteRows(at: [oldIndexPath], with: .automatic)
-    //            tableView.insertRows(at: [newIndexPath], with: .automatic)
-                tableView.moveRow(at: oldIndexPath, to: newIndexPath)
-            case .delete:
-                guard let indexPath = indexPath else { return }
-                tableView.deleteRows(at: [indexPath], with: .automatic)
-            @unknown default:
-                break
-            }
-        }
-    
-}
