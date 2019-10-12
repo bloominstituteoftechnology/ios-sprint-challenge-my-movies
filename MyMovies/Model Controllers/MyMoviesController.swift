eeefb2fddce009c0ad0d5350134053ac1fe1b164
Joel Groomer <joelgroomer@gmail.com>
2019-10-12 11:35:31 -0700

    MyMoviesController.addMovie(movie:), .toggleSeen(movie:), .toggleSeen(representation:), .deleteMovie(movie:) added.

--- MyMovies/Model Controllers/MyMoviesController.swift
+++ MyMovies/Model Controllers/MyMoviesController.swift
@@ -10,26 +10,57 @@ import Foundation
 import CoreData
 
 class MyMoviesController {
-    let baseURL = ""
+    let baseURL = URL(string: "https://lambda-my-movies.firebaseio.com/")!
     typealias CompletionHandler = (Error?) -> Void
     
     
     // MARK: - Local Store Methods
     
     func addMovie(representation: MovieRepresentation) {
+        guard let movie = Movie(representation: representation) else { return }
         
+        put(movie: movie)
+        
+        do {
+            try CoreDataStack.shared.save()
+        } catch {
+            print("Error saving new movie: \(error)")
+        }
     }
     
     func toggleSeen(movie: Movie) {
+        do {
+            movie.hasWatched.toggle()
+            try CoreDataStack.shared.save()
+        } catch {
+            print("Error toggling: \(error)")
+        }
+        
+        put(movie: movie)
         
     }
     
     func toggleSeen(representation: MovieRepresentation) {
-        
+        guard let movie = Movie(representation: representation) else { return }
+        toggleSeen(movie: movie)
     }
     
     func deleteMovie(movie: Movie, completion: @escaping CompletionHandler = { _ in }) {
-        
+        deleteFromServer(movie: movie) { (error) in
+            if let error = error {
+                print("Will not delete local copy")
+                completion(error)
+                return
+            } else {
+                CoreDataStack.shared.mainContext.delete(movie)
+                do {
+                    try CoreDataStack.shared.save()
+                } catch {
+                    print("Error saving after delete: \(error)")
+                }
+                completion(nil)
+            }
+        }
     }
     
     func update(movie: Movie, with representation: MovieRepresentation) {
