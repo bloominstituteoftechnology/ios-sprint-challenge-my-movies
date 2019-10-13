8900a82bb5ed780d06ff78ee6f2d5a8f9a8a52ab
Michael R.F <mrflowers22@uwalumni.com>
2019-10-12 19:00:49 -0600

    created MovieCellTableViewCell's logic and connected the outlets

--- MyMovies/View Controllers/Views/MovieCellTableViewCell.swift
+++ MyMovies/View Controllers/Views/MovieCellTableViewCell.swift
@@ -10,15 +10,29 @@ import UIKit
 
 class MovieCellTableViewCell: UITableViewCell {
 
-    override func awakeFromNib() {
-        super.awakeFromNib()
-        // Initialization code
-    }
-
-    override func setSelected(_ selected: Bool, animated: Bool) {
-        super.setSelected(selected, animated: animated)
-
-        // Configure the view for the selected state
-    }
-
+    var movie: Movie? {
+          didSet {
+              print("MovieTableViewCell: movie was set")
+              updateViews()
+          }
+      }
+      
+      @IBOutlet weak var nameLabel: UILabel!
+      @IBOutlet weak var watchedProperties: UIButton!
+      
+      @IBAction func changeWatchedButton(_ sender: UIButton) {
+           guard let passedInMovie = movie else { print("Error passing in movie in movietableviewcell"); return }
+        
+          //I could make a delegate protocol to handle the toggle. I think that would be more aligned with MVC
+          MyMovieController.shared.toggle(movie: passedInMovie)
+            watchedProperties.setTitle(passedInMovie.hasWatched ? "Watched" : "UnWatched", for: .normal)
+      }
+      
+      private func updateViews(){
+          guard let passedInMovie = movie else { print("Error passing in movie in movietableviewcell"); return }
+          nameLabel.text = passedInMovie.title
+//          let buttonTitle = passedInMovie.hasWatched ? "Watched" : "UnWatched"
+//          watchedProperties.setTitle(buttonTitle, for: .normal)
+         watchedProperties.setTitle(passedInMovie.hasWatched ? "Watched" : "UnWatched", for: .normal)
+      }
 }
