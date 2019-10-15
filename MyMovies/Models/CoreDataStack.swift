c14f47efcd2154653a49de040f5d4522132e341f
Fabiola <saga.fabiola@gmail.com>
2019-10-14 19:10:57 -0500

    Fixed Issue in CoreDataStack

--- MyMovies/Models/CoreDataStack.swift
+++ MyMovies/Models/CoreDataStack.swift
@@ -14,7 +14,7 @@ class CoreDataStack {
     static let shared = CoreDataStack()
     
     lazy var container: NSPersistentContainer = {
-        let container = NSPersistentContainer(name: "TasksV2")
+        let container = NSPersistentContainer(name: "MyMovies")
         container.loadPersistentStores { (_, error) in
             if let error = error {
                 fatalError("Error loading persistent stores: \(error)")
