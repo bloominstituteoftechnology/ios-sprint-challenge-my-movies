ef2abe92b9c4f8cd842e50ad7869d02bdc8e9556
Percy <percival.ngan@gmail.com>
2019-10-18 08:18:12 -0500

    Added Movie+Convenience

--- MyMovies/Models/Movie+Convenience.swift
+++ MyMovies/Models/Movie+Convenience.swift
@@ -9,4 +9,36 @@
 import Foundation
 import CoreData
 
+extension Movie {
 
+	var movieRepresentation: MovieRepresentation? {
+
+		guard let title = title,
+			let identifier = identifier else { return nil }
+			let id = UUID(uuidString: identifier)
+
+		return MovieRepresentation(title: title, identifier: id, hasWatched: hasWatched)
+	}
+
+	@discardableResult convenience init?(title: String,
+					  identifier: String = UUID().uuidString,
+					  hasWatched: Bool = false,
+					  context: NSManagedObjectContext = CoreDataStack.shared.mainContext) {
+
+		self.init(context: context)
+		self.title = title
+		self.identifier = identifier
+		self.hasWatched = hasWatched
+	}
+
+	@discardableResult convenience init?(_ representation: MovieRepresentation,
+										 context: NSManagedObjectContext = CoreDataStack.shared.mainContext) {
+
+		let title = representation.title
+
+		guard let hasWatched =  representation.hasWatched,
+			let identifier = representation.identifier?.uuidString else { return nil}
+
+		self.init(title: title, identifier: identifier, hasWatched: hasWatched, context: context)
+	}
+}
