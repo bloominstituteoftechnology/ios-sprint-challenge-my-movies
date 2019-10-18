d5e2addf2fa77c0e233328c86f36a85a4accfa15
Jerry Haaser <jerry.haaser@hotmail.com>
2019-10-18 12:31:03 -0500

    set up tableview cell

---
+++ MyMovies/MovieTableViewCell.swift
@@ -0,0 +1,33 @@
+//
+//  MovieTableViewCell.swift
+//  MyMovies
+//
+//  Created by admin on 10/18/19.
+//  Copyright © 2019 Lambda School. All rights reserved.
+//
+
+import UIKit
+
+class MovieTableViewCell: UITableViewCell {
+    
+    
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+    
+    @IBAction func watchedButton(_ sender: UIButton) {
+        
+        
+        
+    }
+    
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
