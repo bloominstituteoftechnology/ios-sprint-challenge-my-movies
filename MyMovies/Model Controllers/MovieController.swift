3e3e60b927914accfd0f9235d14ed9a5f29a2fdd
Percy <percival.ngan@gmail.com>
2019-10-18 08:44:00 -0500

    Setup urls

--- MyMovies/Model Controllers/MovieController.swift
+++ MyMovies/Model Controllers/MovieController.swift
@@ -7,12 +7,27 @@
 //
 
 import Foundation
+import CoreData
+
+enum HTTPMethod: String {
+
+	case get = "GET"
+	case put = "PUT"
+	case post = "POST"
+	case delete = "DELETE"
+
+}
 
 class MovieController {
+
+	 // MARK: - Properties
+	var searchedMovies: [MovieRepresentation] = []
     
     private let apiKey = "4cc920dab8b729a619647ccc4d191d5e"
     private let baseURL = URL(string: "https://api.themoviedb.org/3/search/movie")!
-    
+	private let firebaseBaseURL = URL(string: "https://mymovies-c6c11.firebaseio.com/")!
+
+
     func searchForMovie(with searchTerm: String, completion: @escaping (Error?) -> Void) {
         
         var components = URLComponents(url: baseURL, resolvingAgainstBaseURL: true)
@@ -52,7 +67,7 @@ class MovieController {
         }.resume()
     }
     
-    // MARK: - Properties
+
     
     var searchedMovies: [MovieRepresentation] = []
 }
