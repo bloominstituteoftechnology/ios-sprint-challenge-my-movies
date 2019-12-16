805755ca7ed446ee19651cc4f5db1164429d627e
Craig Swanson <cswanson02@me.com>
2019-12-15 21:06:54 -0600

    Created Movie+Convenience file.

---
+++ MyMovies/Models/Movie+Convenience.swift
@@ -0,0 +1,35 @@
+//
+//  Movie+Convenience.swift
+//  MyMovies
+//
+//  Created by Craig Swanson on 12/15/19.
+//  Copyright © 2019 Lambda School. All rights reserved.
+//
+
+import Foundation
+import CoreData
+
+extension Movie {
+    var movieRepresentation: MovieRepresentation? {
+        guard let title = title else { return nil }
+        
+        return MovieRepresentation(title: title, identifier: identifier?.uuidString ?? UUID().uuidString, hasWatched: hasWatched)
+    }
+    
+    convenience init(title: String,
+                     identifier: UUID? = UUID(),
+                     hasWatched: Bool? = false,
+                     context: NSManagedObjectContext = CoreDataStack.shared.mainContext) {
+        self.init(context: context)
+        self.title = title
+        self.identifier = identifier
+        self.hasWatched = hasWatched ?? false
+    }
+    
+    @discardableResult convenience init?(movieRepresentation: MovieRepresentation, context: NSManagedObjectContext = CoreDataStack.shared.mainContext) {
+        guard let identifierString = movieRepresentation.identifier,
+            let identifier = UUID(uuidString: identifierString) else { return nil }
+        
+        self.init(title: movieRepresentation.title, identifier: identifier, hasWatched: movieRepresentation.hasWatched, context: context)
+    }
+}
