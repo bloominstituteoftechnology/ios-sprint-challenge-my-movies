c6e087c7a41ffa262ab06d53718f0330d2ab4cef
Jessie <threedaystoryllc@icloud.com>
2019-12-15 21:48:54 -0500

    Initial Commit
    Movie+Convenience, Core Data Stack and Data Model added

---
+++ ios-sprint-challenge-my-movies PT3/MyMovies/Models/CoreDataStack.swift
@@ -0,0 +1,38 @@
+//
+//  CoreDataStack.swift
+//  MyMovies
+//
+//  Created by Jessie Ann Griffin on 12/15/19.
+//  Copyright © 2019 Lambda School. All rights reserved.
+//
+
+import Foundation
+import CoreData
+
+class CoreDataStack {
+    static let shared = CoreDataStack()
+    
+    lazy var container: NSPersistentContainer = {
+        let container = NSPersistentContainer(name: "Movie")
+        container.loadPersistentStores { _, error in
+            if let error = error {
+                fatalError("Failed to load persistent stores: \(error)")
+            }
+        }
+        container.viewContext.automaticallyMergesChangesFromParent = true
+        return container
+    }()
+    
+    var mainContext: NSManagedObjectContext { return container.viewContext }
+    
+    func save(context: NSManagedObjectContext = CoreDataStack.shared.mainContext) {
+        context.performAndWait {
+            do {
+                try context.save()
+            } catch {
+                NSLog("Unable to save context: \(error)")
+                context.reset()
+            }
+        }
+    }
+}
---
+++ ios-sprint-challenge-my-movies PT3/MyMovies/Models/Movie+Convenience.swift
@@ -0,0 +1,39 @@
+//
+//  Movie+Convenience.swift
+//  MyMovies
+//
+//  Created by Jessie Ann Griffin on 12/15/19.
+//  Copyright © 2019 Lambda School. All rights reserved.
+//
+
+import Foundation
+import CoreData
+
+extension Movie {
+    
+    convenience init(title: String,
+                     identifier: UUID? = UUID(),
+                     hasWatched: Bool? = false,
+                     context: NSManagedObjectContext = CoreDataStack.shared.mainContext) {
+        self.init(context: context)
+        self.title = title
+        self.identifier = identifier
+        
+        guard let hasWatched = hasWatched else { return }
+        self.hasWatched = hasWatched
+    }
+    
+    @discardableResult convenience init?(movieRepresentation: MovieRepresentation,
+                                         context: NSManagedObjectContext = CoreDataStack.shared.mainContext) {
+        
+        self.init(title: movieRepresentation.title,
+                  identifier: movieRepresentation.identifier,
+                  hasWatched: movieRepresentation.hasWatched,
+                  context: context)
+    }
+    
+    var movieRepresentation: MovieRepresentation? {
+        guard let title = title else { return nil }
+        return MovieRepresentation(title: title, identifier: identifier, hasWatched: hasWatched)
+    }
+}
