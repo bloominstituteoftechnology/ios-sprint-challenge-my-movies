78c8bc7d91c9705c11270ec684eeba227117894e
Tyler Christian <tylerc71197@gmail.com>
2019-12-20 14:05:46 -0500

    Still working in the movieController file not yet done

--- MyMovies/Model Controllers/MovieController.swift
+++ MyMovies/Model Controllers/MovieController.swift
@@ -5,13 +5,23 @@
 //  Created by Spencer Curtis on 8/17/18.
 //  Copyright © 2018 Lambda School. All rights reserved.
 //
-
+import CoreData
 import Foundation
+
+enum HTTPMethod: String {
+    case get = "GET"
+    case put = "PUT"
+    case post = "POST"
+    case delete = "DELETE"
+}
 
 class MovieController {
     
+    typealias CompletionHandler = (Error?) -> Void
+    
     private let apiKey = "4cc920dab8b729a619647ccc4d191d5e"
     private let baseURL = URL(string: "https://api.themoviedb.org/3/search/movie")!
+    private let fireBaseURL = URL(string: "https://mymovies-e959b.firebaseio.com/")!
     
     func searchForMovie(with searchTerm: String, completion: @escaping (Error?) -> Void) {
         
@@ -50,6 +60,87 @@ class MovieController {
                 completion(error)
             }
         }.resume()
+    }
+    
+    
+    private func put(movie: Movie, completion: @escaping CompletionHandler = {_ in }) {
+        let requestURL = fireBaseURL.appendingPathComponent(movie.identifier?.uuidString ?? UUID().uuidString).appendingPathExtension("json")
+        var request = URLRequest(url: requestURL)
+        request.httpMethod = HTTPMethod.put.rawValue
+        
+        guard let title = movie.title else { return }
+        let rep = MovieRepresentation(title: title, identifier: movie.identifier, hasWatched: movie.hasWatched)
+        
+        do {
+            request.httpBody = try JSONEncoder().encode(rep)
+        } catch {
+            print("Error could not encode movie: \(error)")
+            completion(error)
+            return
+        }
+        
+        URLSession.shared.dataTask(with: request) { (data, _, error) in
+            if let error = error {
+                print("Error putting Movie to server: \(error)")
+                completion(error)
+                return
+            }
+            completion(nil)
+        }.resume()
+    }
+    
+    
+    func createMovie(with title: String, identifier: UUID, hasWatched: Bool) {
+        let movie = Movie(title: title, identifier: identifier, hasWatched: hasWatched)
+        put(movie: movie)
+        try? CoreDataStack.shared.save()
+    }
+    
+    func deleteMovieFromServer(movie: Movie, completion: @escaping CompletionHandler = {_ in}) {
+        guard let identifier = movie.identifier else {
+            print("Movie identifier is nil")
+            completion(NSError())
+            return
+        }
+        let requestURL = fireBaseURL.appendingPathComponent(identifier.uuidString).appendingPathExtension("json")
+        var request = URLRequest(url: requestURL)
+        request.httpMethod = "DELETE"
+        
+        URLSession.shared.dataTask(with: request) { _, response, error in
+            print(response!)
+            DispatchQueue.main.async {
+                completion(error)
+            }
+        }.resume()
+    }
+    
+    func updateMovies(with reoresentation: [MovieRepresentation]) throws {
+        let moviesWithId = reoresentation.filter { $0.identifier != nil }
+        let identifiersToFetch = moviesWithId.compactMap { $0.identifier! }
+        let repByID = Dictionary(uniqueKeysWithValues: zip(identifiersToFetch, moviesWithId))
+        
+        var moviesToCreate = repByID
+        
+        let fetchRequest: NSFetchRequest<Movie> = Movie.fetchRequest()
+        fetchRequest.predicate = NSPredicate(format: "identifier IN %@", identifiersToFetch)
+        
+        let context = CoreDataStack.shared.container.newBackgroundContext()
+        context.perform {
+            do {
+                let existingMovies = try context.fetch(fetchRequest)
+                for movie in existingMovies {
+                    guard let id = movie.identifier, let representation = repByID[id] else { continue }
+                    
+                    
+                }
+            } catch {
+                print("Error fetching movies: \(error)")
+            }
+        }
+    }
+    
+    private func update(movie: Movie, with representation: MovieRepresentation) {
+        movie.title = 
     }
     
     // MARK: - Properties
