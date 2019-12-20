f2eec5b24dcc6fd45de918126536c3fdfa435279
Zack Larsen <zacklarsen1@gmail.com>
2019-12-20 11:55:54 -0700

    step 1 done

---
+++ MyMovies/View Controllers/MoviesTableViewCell.swift
@@ -0,0 +1,21 @@
+//
+//  MoviesTableViewCell.swift
+//  MyMovies
+//
+//  Created by Zack Larsen on 12/20/19.
+//  Copyright © 2019 Lambda School. All rights reserved.
+//
+
+import UIKit
+
+class MoviesTableViewCell: UITableViewCell {
+    
+    var movie: Movie?
+
+    weak var delegate: 
+}
+@IBOutlet saveMovie: UIButton!
+
+@IBAction func saveMovie(_ sender: UIButton) {
+    
+}
