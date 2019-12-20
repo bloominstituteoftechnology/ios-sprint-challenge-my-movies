3e232213aa733d1a67279f7861b02964a1ed531e
Chad Rutherford <chad_rutherford@outlook.com>
2019-12-20 11:17:52 -0500

    Added NSManagedObject Model and CoreData Stack

---
+++ MyMovies/Models/CoreDataStack.swift
@@ -0,0 +1,43 @@
+//
+//  CoreDataStack.swift
+//  MyMovies
+//
+//  Created by Chad Rutherford on 12/20/19.
+//  Copyright © 2019 Lambda School. All rights reserved.
+//
+
+import CoreData
+import Foundation
+
+class CoreDataStack {
+    static let shared = CoreDataStack()
+    
+    var container: NSPersistentContainer = {
+        let container = NSPersistentContainer(name: "Movie")
+        container.loadPersistentStores { _, error in
+            if let error = error {
+                fatalError("Error loading persistent stores: \(error.localizedDescription)")
+            }
+        }
+        container.viewContext.automaticallyMergesChangesFromParent = true
+        return container
+    }()
+    
+    var mainContext: NSManagedObjectContext {
+        return container.viewContext
+    }
+    
+    func save(context: NSManagedObjectContext = CoreDataStack.shared.mainContext) throws {
+        var error: Error?
+        
+        context.performAndWait {
+            do {
+                try context.save()
+            } catch let saveError {
+                error = saveError
+            }
+        }
+        
+        if let error = error { throw error }
+    }
+}
---
+++ MyMovies/Resources/Constants/Constants.swift
@@ -0,0 +1,13 @@
+//
+//  Constants.swift
+//  MyMovies
+//
+//  Created by Chad Rutherford on 12/20/19.
+//  Copyright © 2019 Lambda School. All rights reserved.
+//
+
+import Foundation
+
+struct Networking {
+    static let baseURL = "https://mymovies-f1ecb.firebaseio.com/"
+}
