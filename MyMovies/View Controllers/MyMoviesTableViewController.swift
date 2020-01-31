8647a561e8b6ae3319d70fbaa87691391e30ac5c
Zack Larsen <zacklarsen1@gmail.com>
2020-01-31 12:00:48 -0700

    not quite finished

--- MyMovies/View Controllers/MyMoviesTableViewController.swift
+++ MyMovies/View Controllers/MyMoviesTableViewController.swift
@@ -67,17 +67,21 @@ class MyMoviesTableViewController: UITableViewController {
     }
     */
 
-    /*
+    
     // Override to support editing the table view.
     override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath) {
         if editingStyle == .delete {
+            
+            let movie = fetchedResultsController.object(at: indexPath)
+        
+            movieController.delete
             // Delete the row from the data source
             tableView.deleteRows(at: [indexPath], with: .fade)
         } else if editingStyle == .insert {
             // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
         }    
     }
-    */
+    
 
     /*
     // Override to support rearranging the table view.
