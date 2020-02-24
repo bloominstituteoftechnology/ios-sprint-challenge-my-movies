ba2dad6d307be7b12f542697cec5eebba290e6df
Joshua Rutkowski <jr.rtkski@gmail.com>
2020-02-23 21:37:37 -0500

    updated MovieSearchTableViewController

---
+++ MyMovies/Models/CoreDataStack.swift
@@ -0,0 +1,50 @@
+//
+//  CoreDataStack.swift
+//  MyMovies
+//
+//  Created by Joshua Rutkowski on 2/23/20.
+//  Copyright © 2020 Lambda School. All rights reserved.
+//
+
+import Foundation
+import CoreData
+
+class CoreDataStack {
+    
+    // Let us access the CoreDataStack from anywhere in the app.
+    static let shared = CoreDataStack()
+    
+    // Set up a persistent container
+    
+    lazy var container: NSPersistentContainer = {
+        
+        let container = NSPersistentContainer(name: "MyMovies")
+        
+        container.loadPersistentStores { (_, error) in
+            if let error = error {
+                fatalError("Failed to load persistent stores: \(error)")
+            }
+        }
+        container.viewContext.automaticallyMergesChangesFromParent = true
+        return container
+    }()
+    
+    // Create easy access to the moc (managed object context)
+    var mainContext: NSManagedObjectContext {
+        return container.viewContext
+    }
+    
+    func save(context: NSManagedObjectContext) throws {
+        var error: Error?
+        
+        context.performAndWait {
+            do {
+                try context.save()
+            } catch let saveError {
+                error = saveError
+            }
+        }
+        
+        if let error = error { throw error }
+    }
+}
