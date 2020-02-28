a586bb6bf2d27227618f256c4f8c986df994fdbc
Keri Levesque <kerianne166@gmail.com>
2020-02-28 11:19:29 -0500

    added model files

---
+++ MyMovies/Model Controllers/MyMoviesController.swift
@@ -0,0 +1,9 @@
+//
+//  MyMoviesController.swift
+//  MyMovies
+//
+//  Created by Keri Levesque on 2/28/20.
+//  Copyright © 2020 Lambda School. All rights reserved.
+//
+
+import Foundation
---
+++ MyMovies/Models/Movie+Convenience.swift
@@ -0,0 +1,10 @@
+//
+//  Movie+Convenience.swift
+//  MyMovies
+//
+//  Created by Keri Levesque on 2/28/20.
+//  Copyright © 2020 Lambda School. All rights reserved.
+//
+
+import Foundation
+
