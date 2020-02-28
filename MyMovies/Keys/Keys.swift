bb3c5dd52be34274b9f213d61fa1428364230830
Chris <chris.gonzales.82@gmail.com>
2020-02-28 08:49:41 -0800

    CoreDataStack created and Keys file created

---
+++ MyMovies/Keys/Keys.swift
@@ -0,0 +1,13 @@
+//
+//  Keys.swift
+//  MyMovies
+//
+//  Created by Chris Gonzales on 2/28/20.
+//  Copyright © 2020 Lambda School. All rights reserved.
+//
+
+import Foundation
+
+struct Keys {
+    static let persistenceContainer = "MyMoviesCoreData"
+}
