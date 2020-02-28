ebadaf3c5bab185f85f6003d8fda99c150962c18
Enrique Gongora <EGTech24@gmail.com>
2020-02-28 08:33:34 -0800

    CoreDataStack and Movie+Convenience completed.

--- MyMovies/Models/CoreDataStack.swift
+++ MyMovies/Models/CoreDataStack.swift
@@ -7,3 +7,37 @@
 //
 
 import Foundation
+import CoreData
+
+class CoreDataStack {
+    
+    static let shared = CoreDataStack()
+    
+    lazy var container: NSPersistentContainer = {
+        // The name below should match the filename of the xcdatamodeld file exactly (minus the extension)
+        let container = NSPersistentContainer(name: "MyMovies")
+        container.loadPersistentStores { _, error in
+            if let error = error {
+                fatalError("Failed to load persistent stores: \(error)")
+            }
+        }
+        container.viewContext.automaticallyMergesChangesFromParent = true
+        return container
+    }()
+    
+    var mainContext: NSManagedObjectContext {
+        return container.viewContext
+    }
+    
+    func save(context: NSManagedObjectContext = CoreDataStack.shared.mainContext) throws {
+        var error: Error?
+        context.performAndWait {
+            do {
+                try context.save()
+            } catch let saveError {
+                error = saveError
+            }
+        }
+        if let error = error { throw error }
+    }
+}
--- MyMovies/Models/Movie+Convenience.swift
+++ MyMovies/Models/Movie+Convenience.swift
@@ -7,3 +7,34 @@
 //
 
 import Foundation
+import CoreData
+
+extension Movie {
+    
+    // This computed property allows any managed object to become a MovieRepresentation for sending to Firebase
+    var movieRepresentation: MovieRepresentation? {
+        guard let title = title else { return nil }
+        if identifier == nil {
+            identifier = UUID()
+        }
+        
+        return MovieRepresentation(title: title, identifier: identifier, hasWatched: hasWatched)
+    }
+    
+    // This created a new managed object from raw data
+    @discardableResult convenience init(title: String, identifier: UUID = UUID(), hasWatched: Bool = false, context: NSManagedObjectContext = CoreDataStack.shared.mainContext) {
+        self.init(context: context)
+        self.title = title
+        self.identifier = identifier
+        self.hasWatched = hasWatched
+    }
+    
+    // This creates a managed object from a MovieRepresentation object (which comes from Firebase)
+    @discardableResult convenience init?(movieRepresentation: MovieRepresentation, context: NSManagedObjectContext = CoreDataStack.shared.mainContext) {
+        guard let identifier = movieRepresentation.identifier, let hasWatched = movieRepresentation.hasWatched else { return nil }
+        self.init(title: movieRepresentation.title,
+                  identifier: identifier,
+                  hasWatched: hasWatched,
+                  context: context)
+    }
+}
