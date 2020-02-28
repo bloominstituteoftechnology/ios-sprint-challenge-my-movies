4e9d2648eba6bcb4e828646d724f25abf2d0846c
elizabeth paige <wingateep@Icloud.com>
2020-02-28 11:35:39 -0500

    set up coredata stack + xcdatamodel

---
+++ MyMovies/Models/Core Data -/CoreDataStack.swift
@@ -0,0 +1,32 @@
+//
+//  CoreDataStack.swift
+//  MyMovies
+//
+//  Created by Elizabeth Wingate on 2/28/20.
+//  Copyright © 2020 Lambda School. All rights reserved.
+//
+
+import Foundation
+import CoreData
+import UIKit
+
+class CoreDataStack {
+    
+    static let shared = CoreDataStack()
+      let container: NSPersistentContainer
+      
+      let mainContext: NSManagedObjectContext
+      
+      init() {
+        container = NSPersistentContainer(name: "MyMovies")
+          
+        container.loadPersistentStores { (description, error) in
+          if let e = error {
+            fatalError("Couldn't load the data store: \(e)")
+        }
+    }
+        mainContext = container.viewContext
+              
+        mainContext.automaticallyMergesChangesFromParent = true
+  }
+}
---
+++ MyMovies/Models/Movie+Convenience.swift
@@ -0,0 +1,9 @@
+//
+//  Movie+Convenience.swift
+//  MyMovies
+//
+//  Created by Elizabeth Wingate on 2/28/20.
+//  Copyright © 2020 Lambda School. All rights reserved.
+//
+
+import Foundation
