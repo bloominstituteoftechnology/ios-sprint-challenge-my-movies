3dfd484c643f65034f5b7727757aee94d90dc4bc
Bharat Kumar <Bharatiosdeveloper@gmail.com>
2020-03-27 16:11:29 -0700

    Added header and delegate to thhe my movies view controller

--- MyMovies/View Controllers/MyMoviesTableViewController.swift
+++ MyMovies/View Controllers/MyMoviesTableViewController.swift
@@ -8,7 +8,7 @@
 
 import UIKit
 import CoreData
-class MyMoviesTableViewController: UITableViewController, NSFetchedResultsControllerDelegate {
+class MyMoviesTableViewController: UITableViewController{
     
 let movieController = MovieController()
     
@@ -38,23 +38,25 @@ let movieController = MovieController()
 
     override func numberOfSections(in tableView: UITableView) -> Int {
         // #warning Incomplete implementation, return the number of sections
-        return 0
+       return fetchedResultsController.sections?.count ?? 1
     }
 
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         // #warning Incomplete implementation, return the number of rows
-        return 0
+        return fetchedResultsController.sections?[section].numberOfObjects ?? 0
     }
 
-    /*
+
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath)
+       guard let cell = tableView.dequeueReusableCell(withIdentifier: "MyMovieCell", for: indexPath) as? AddedMoviesTableViewCell else { return UITableViewCell() }
+               let movie = fetchedResultsController.object(at: indexPath)
+               
+               cell.movie = movie
+               cell.delegate = self
 
-        // Configure the cell...
-
-        return cell
+               return cell
     }
-    */
+
 
     /*
     // Override to support conditional editing of the table view.
@@ -64,17 +66,29 @@ let movieController = MovieController()
     }
     */
 
-    /*
+    
     // Override to support editing the table view.
-    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath) {
+    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
         if editingStyle == .delete {
-            // Delete the row from the data source
-            tableView.deleteRows(at: [indexPath], with: .fade)
+            let movie = fetchedResultsController.object(at: indexPath)
+            movieController.delete(movie: movie)
         } else if editingStyle == .insert {
             // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
         }    
     }
-    */
+    
+    override func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
+         guard let newSection = fetchedResultsController.sections?[section] else { return nil }
+               switch newSection.name {
+                   case "1":
+                       return "Watched"
+                   case "0":
+                       return "Not Watched"
+                   default:
+                    return nil
+               }
+    }
+    
 
     /*
     // Override to support rearranging the table view.
@@ -102,3 +116,63 @@ let movieController = MovieController()
     */
 
 }
+extension MyMoviesTableViewController: NSFetchedResultsControllerDelegate {
+    //     this is the warning the tableview that the fetch controller is goijng to makechanges in the tableview.
+    func controllerWillChangeContent(_ controller: NSFetchedResultsController<NSFetchRequestResult>) {
+        tableView.beginUpdates()
+        //        this is the beggnining of the fetchhing
+    }
+    func controllerDidChangeContent(_ controller: NSFetchedResultsController<NSFetchRequestResult>) {
+        tableView.endUpdates()
+        //         the endo of the fetchhing.
+    }
+    //    deletes the entire section or insert entire section
+    func controller(_ controller: NSFetchedResultsController<NSFetchRequestResult>,
+                    didChange sectionInfo: NSFetchedResultsSectionInfo,
+                    atSectionIndex sectionIndex: Int,
+                    for type: NSFetchedResultsChangeType) {
+        switch type {
+        case .insert:
+            tableView.insertSections(IndexSet(integer: sectionIndex), with: .automatic)
+        case .delete:
+            tableView.deleteSections(IndexSet(integer: sectionIndex), with: .automatic)
+        default:
+            break
+        }
+    }
+    func controller(_ controller: NSFetchedResultsController<NSFetchRequestResult>,
+                    didChange anObject: Any,
+                    at indexPath: IndexPath?,
+                    for type: NSFetchedResultsChangeType,
+                    newIndexPath: IndexPath?) {
+        switch type {
+        case .insert:
+            guard let newIndexPath = newIndexPath else { return }
+            tableView.insertRows(at: [newIndexPath], with: .automatic)
+        case .update:
+            guard let indexPath = indexPath else { return }
+            tableView.reloadRows(at: [indexPath], with: .automatic)
+        case .move:
+            guard let oldIndexPath = indexPath,
+                let newIndexPath = newIndexPath else { return }
+            tableView.deleteRows(at: [oldIndexPath], with: .automatic)
+            tableView.insertRows(at: [newIndexPath], with: .automatic)
+        case .delete:
+            guard let indexPath = indexPath else { return }
+            tableView.deleteRows(at: [indexPath], with: .automatic)
+        @unknown default:
+            break
+        }
+    }
+}
+extension MyMoviesTableViewController: AddedMoviesTableViewCellDelegate {
+    func itHasWatched(to cell: AddedMoviesTableViewCell) {
+        if let movie = cell.movie {
+            movie.hasWatched.toggle()
+            movieController.updateMovie(for: movie)
+        }
+            
+    }
+}
+
+
