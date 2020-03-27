e0a2bb3cb9efc34dd147bb30540b2c127943f959
Bharat Kumar <Bharatiosdeveloper@gmail.com>
2020-03-27 09:50:54 -0700

    Added movie+convenience set up

--- MyMovies/Models/Movie+Convenience.swift
+++ MyMovies/Models/Movie+Convenience.swift
@@ -2,8 +2,46 @@
 //  Movie+Convenience.swift
 //  MyMovies
 //
-//  Created by Bhawnish Kumar on 3/27/20.
+//  Created by David Wright on 2/23/20.
 //  Copyright © 2020 Lambda School. All rights reserved.
 //
 
 import Foundation
+import CoreData
+
+extension Movie {
+    var movieRepresentation: MovieRepresentation? {
+        guard let title = title else { return nil }
+        
+        if identifier == nil {
+            identifier = UUID()
+        }
+        
+        return MovieRepresentation(title: title, identifier: identifier, hasWatched: hasWatched)
+    }
+    
+    @discardableResult
+    convenience init(title: String,
+                     identifier: UUID = UUID(),
+                     hasWatched: Bool = false,
+                     context: NSManagedObjectContext = CoreDataStack.shared.mainContext) {
+        self.init(context: context)
+        self.title = title
+        self.identifier = identifier
+        self.hasWatched = hasWatched
+    }
+    
+    @discardableResult
+    convenience init?(movieRepresentation: MovieRepresentation,
+                      context: NSManagedObjectContext = CoreDataStack.shared.mainContext) {
+                      let hasWatched = movieRepresentation.hasWatched ?? false
+                      let movieIdentifier = movieRepresentation.identifier ?? UUID()
+        
+        
+        
+        self.init(title: movieRepresentation.title,
+                  identifier: movieIdentifier,
+                  hasWatched: hasWatched,
+                  context: context)
+    }
+}
