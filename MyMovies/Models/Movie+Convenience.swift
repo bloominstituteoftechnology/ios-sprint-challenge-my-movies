1e1408c2194d6aec8df966857f2f58617b5e93ad
Hector L <hectoremil21@hotmail.com>
2020-03-27 11:32:15 -0400

    Initializer to convert from representation to core data, and computed property.

--- MyMovies/Models/Movie+Convenience.swift
+++ MyMovies/Models/Movie+Convenience.swift
@@ -11,11 +11,22 @@ import CoreData
 
 extension Movie {
     
+    
+    var movieRepresentation: MovieRepresentation? {
+        guard let title = title else { return nil}
+        return MovieRepresentation(title: title, identifier: identifier, hasWatched: hasWatched)
+    }
+    
     // Convenience initializer for creating a movie directly into coreData. May never be used, but gotta get those reps in you feel me.
     @discardableResult convenience init (identifier: UUID = UUID(), title: String, hasWatched: Bool = false, context: NSManagedObjectContext = CoreDataStack.shared.mainContext) {
         self.init(context: context)
         self.identifier = identifier
         self.title = title
         self.hasWatched = hasWatched
+    }
+    
+    //  Failable convenience initializer for converting a representation into core data.
+    @discardableResult convenience init?(representation: MovieRepresentation, context: NSManagedObjectContext = CoreDataStack.shared.mainContext) {
+        self.init(identifier: representation.identifier ?? UUID(), title: representation.title, hasWatched: representation.hasWatched ?? false, context: context)
     }
 }
