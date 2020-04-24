499be318d502b3f46aea8f1e750dec3baf353ec4
Shawn <shawn.james@me.com>
2020-04-24 11:16:32 -0400

    add movieSearchTableViewButton for custom cell and create correlating tableviewcell file

---
+++ MyMovies/Views (tableViewCells)/MovieSearchTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  MovieSearchTableViewCell.swift
+//  MyMovies
+//
+//  Created by Shawn James on 4/24/20.
+//  Copyright © 2020 Lambda School. All rights reserved.
+//
+
+import UIKit
+
+class MovieSearchTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
