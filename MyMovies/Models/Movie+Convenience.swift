0803bbd64dfabfdc23cb915265aed67d7da094a5
Shawn <shawn.james@me.com>
2020-04-24 13:17:11 -0400

    add broken convenience init file

---
+++ MyMovies/Models/Movie+Convenience.swift
@@ -0,0 +1,40 @@
+//
+//  Movie+Convenience.swift
+//  MyMovies
+//
+//  Created by Shawn James on 4/24/20.
+//  Copyright © 2020 Lambda School. All rights reserved.
+//
+
+import Foundation
+import CoreData
+
+extension Movie {
+    
+    var movieRepresentation: MovieRepresentation? {
+        guard let id = identifier,
+        let title = title else { return nil }
+        
+        return MovieRepresentation(title: title,
+                                   identifier: id,
+                                   hasWatched: hasWatched)
+    }
+    
+    @discardableResult convenience init(identifier: UUID = UUID(),
+                                        title: String,
+                                        hasWatched: Bool = false,
+                                        context: NSManagedObjectContext = CoreDataStack.shared.mainContext) {
+        self.init(context: context)
+        self.identifier = identifier
+        self.title = title
+        self.hasWatched = hasWatched
+    }
+    
+    @discardableResult convenience init?(movieRepresentation: MovieRepresentation, context: NSManagedObjectContext = CoreDataStack.shared.mainContext) {
+        
+        self.init(identifier: identifier,
+                  title = movieRepresentation.title,
+                  hasWatched = movieRepresentation.hasWatched ?? false)
+    }
+    
+}
--- MyMovies/View Controllers/MyMoviesTableViewController.swift
+++ MyMovies/View Controllers/MyMoviesTableViewController.swift
@@ -10,14 +10,12 @@ import UIKit
 
 class MyMoviesTableViewController: UITableViewController {
 
+    let movieController = MovieController()
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Uncomment the following line to preserve selection between presentations
-        // self.clearsSelectionOnViewWillAppear = false
-
-        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
-        // self.navigationItem.rightBarButtonItem = self.editButtonItem
     }
 
     override func didReceiveMemoryWarning() {
