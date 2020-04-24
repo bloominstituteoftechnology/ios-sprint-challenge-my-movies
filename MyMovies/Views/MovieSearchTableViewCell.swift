7e003303d0c672bdc956a9f67bbdc34734b1b677
Nichole Davidson <nicholedavid@gmail.com>
2020-04-24 09:32:26 -0600

    Added concurrency debug scheme, movieSearchTableViewCell, label, and button. Wired label and button

---
+++ MyMovies/Views/MovieSearchTableViewCell.swift
@@ -0,0 +1,30 @@
+//
+//  MovieSearchTableViewCell.swift
+//  MyMovies
+//
+//  Created by Nichole Davidson on 4/24/20.
+//  Copyright © 2020 Lambda School. All rights reserved.
+//
+import Foundation
+import UIKit
+
+class MovieSearchTableViewCell: UITableViewCell {
+
+    @IBOutlet weak var movieSearchTitle: UILabel!
+    @IBOutlet weak var addMovieButton: UIButton!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    @IBAction func addMovie() {
+        
+    }
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
