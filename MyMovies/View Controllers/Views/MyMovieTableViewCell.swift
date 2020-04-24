9795460fedd6975e84ef3d3df896c38c22fb11ee
Harmony Radley <harmonyradley99@gmail.com>
2020-04-24 16:12:07 -0400

    Added Outlets and Actions

--- MyMovies/View Controllers/Views/MyMovieTableViewCell.swift
+++ MyMovies/View Controllers/Views/MyMovieTableViewCell.swift
@@ -9,13 +9,42 @@
 import UIKit
 
 class MyMovieTableViewCell: UITableViewCell {
-
+    
     static let reuseIdentifier = "MyMovieCell"
     
+    var movie: Movie? {
+        didSet {
+            updateViews()
+        }
+    }
+    
+    // MARK: - Outlets
+    
+    @IBOutlet weak var movieTitleLabel: UILabel!
+    @IBOutlet weak var hasWatchedButtonTapped: UIButton!
+    
+    // MARK: - Action
+    
+    @IBAction func hasWatched(_ sender: UIButton) {
+        guard let movie = movie else { return }
+        
+        movie.hasWatched.toggle()
+        
+        sender.setTitle(movie.hasWatched ? "Unwatched" : "Watched", for: .normal)
+        
+        do {
+            try CoreDataStack.shared.mainContext.save()
+        } catch {
+            NSLog("Error saving managed object context: \(error)")
+        }
+    }
+    
+    private func updateViews() {
+        guard let movie = movie else { return }
+        
+        movieTitleLabel.text = movie.title
+        hasWatchedButtonTapped.setTitle(movie.hasWatched ? "Unwatched" : "Watched", for: .normal)
+    }
     
     
-    
-    
-    
-
 }
