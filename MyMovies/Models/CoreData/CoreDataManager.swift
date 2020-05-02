b9b6d70a5015183528e89dcfa44b5fd50cf8b208
Shawn James <shawn.james@me.com>
2020-05-02 14:03:59 -0400

    add Core Data Stack

---
+++ MyMovies/Models/CoreData/CoreDataManager.swift
@@ -0,0 +1,35 @@
+//
+//  CoreDataManager.swift
+//  MyMovies
+//
+//  Created by Shawn James on 5/2/20.
+//  Copyright © 2020 Lambda School. All rights reserved.
+//
+
+import Foundation
+import CoreData
+
+class CoreDataManager {
+    
+    // this is how you make a core data stack
+    
+    // create singleton
+    static let shared = CoreDataManager()
+    
+    // create container
+    lazy var persistentContainer: NSPersistentContainer = {
+        let container = NSPersistentContainer(name: "Tasks")
+        container.loadPersistentStores { (_, error) in
+            if let error = error {
+                fatalError("Failed to load persistent stores: \(error)")
+            }
+        }
+        return container
+    }()
+    
+    // create context -> everything done in the app uses the context
+    var mainContext: NSManagedObjectContext {
+        persistentContainer.viewContext
+    }
+    
+}
---
+++ MyMovies/Models/Movie+Convenience.swift
@@ -0,0 +1,16 @@
+//
+//  Movie+Convenience.swift
+//  MyMovies
+//
+//  Created by Shawn James on 5/2/20.
+//  Copyright © 2020 Lambda School. All rights reserved.
+//
+
+import Foundation
+import CoreData
+
+extension Movie {
+    
+    
+    
+}
