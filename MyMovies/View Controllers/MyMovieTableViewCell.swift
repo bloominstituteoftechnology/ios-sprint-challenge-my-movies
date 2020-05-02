fa9fe762fc3718044fc17301fc73b372fb8f3f0d
Claudia Maciel <clc_80@yahoo.com>
2020-05-02 11:18:50 -0700

    User is able to see movies my movies and toggle between watched and not

--- MyMovies/View Controllers/MyMovieTableViewCell.swift
+++ MyMovies/View Controllers/MyMovieTableViewCell.swift
@@ -12,11 +12,18 @@ class MyMovieTableViewCell: UITableViewCell {
 
     // MARK: IBOutlets
     @IBOutlet var movieTitleLabel: UILabel!
+    @IBOutlet var hasWatchedButton: UIButton!
+    
     
     //MARK: Properties
-    var movie: Movie?
+    var movie: Movie? {
+        didSet {
+            updateViews()
+        }
+    }
     var movieController: MovieFirebaseController?
     
+    // MARK: - Do we need these?
     override func awakeFromNib() {
         super.awakeFromNib()
         // Initialization code
@@ -27,9 +34,30 @@ class MyMovieTableViewCell: UITableViewCell {
 
         // Configure the view for the selected state
     }
+    
+    // MARK: - Functions
+    
+    func updateViews() {
+        guard let movie = movie else { return }
+        movieTitleLabel.text = movie.title
+        
+        if movie.hasWatched {
+            hasWatchedButton.setTitle("Watched", for: .normal)
+        } else {
+            hasWatchedButton.setTitle("Not Watched", for: .normal)
+        }
+    }
 
     // MARK: - IBAction
     @IBAction func hasWatchedButtonPressed(_ sender: Any) {
+        movie?.hasWatched.toggle()
+        
+        updateViews()
+        
+        do {
+            try CoreDataStack.shared.mainContext.save()
+        } catch {
+            NSLog("Error saving managed object context: \(error)")
+        }
     }
-    
 }
