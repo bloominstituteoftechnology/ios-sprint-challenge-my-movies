8595fd92219e224bccf9d5f5535b7a7cd8f3b531
Chad Parker <chad@chadparker.com>
2020-05-01 19:06:17 -0700

    Add CoreDataStack

---
+++ MyMovies/Models/CoreDataStack.swift
@@ -0,0 +1,47 @@
+//
+//  CoreDataStack.swift
+//  MyMovies
+//
+//  Created by Chad Parker on 5/1/20.
+//  Copyright © 2020 Lambda School. All rights reserved.
+//
+
+import Foundation
+import CoreData
+
+class CoreDataStack {
+    
+    static let shared = CoreDataStack()
+    
+    lazy var container: NSPersistentContainer = {
+        let container = NSPersistentContainer(name: "Journal")
+        container.loadPersistentStores { _, error in
+            if let error = error {
+                fatalError("Failed to load persistent stores: \(error)")
+            }
+        }
+        return container
+    }()
+    
+    // makes access to the context easier
+    // reminds you to use the context on the main queue
+    var mainContext: NSManagedObjectContext {
+        return container.viewContext
+    }
+    
+    func save(context: NSManagedObjectContext = CoreDataStack.shared.mainContext) throws {
+        var error: Error?
+        
+        context.performAndWait {
+            do {
+                try context.save()
+            } catch let saveError {
+                error = saveError
+            }
+        }
+        
+        if let error = error {
+            throw error
+        }
+    }
+}
