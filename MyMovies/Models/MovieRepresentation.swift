ef2b0371a090c5032cdbc9495b5f3e91b115768a
Libby Thomas <thomaslibby05@gmail.com>
2020-05-01 20:05:37 -0500

    Initial Commit

--- MyMovies/Models/MovieRepresentation.swift
+++ MyMovies/Models/MovieRepresentation.swift
@@ -9,7 +9,7 @@
 import Foundation
 
 struct MovieRepresentation: Equatable, Codable {
-    let title: String
+    let title: String 
     
     /*
      identifier and hasWatched are not a part of The Movie DB API, however they will be used both on Firebase and on the application itself.
