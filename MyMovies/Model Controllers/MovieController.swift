c1edbac038c55c50acd9544e5283b056599ead36
Matt Martindale <martindalematt18@yahoo.com>
2020-05-03 19:01:36 -0600

    Initial commit

--- MyMovies/Model Controllers/MovieController.swift
+++ MyMovies/Model Controllers/MovieController.swift
@@ -2,7 +2,7 @@
 //  MovieController.swift
 //  MyMovies
 //
-//  Created by Spencer Curtis on 8/17/18.
+//  Created by Matt Martindale on 8/17/18.
 //  Copyright © 2018 Lambda School. All rights reserved.
 //
 
