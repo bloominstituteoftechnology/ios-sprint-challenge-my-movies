d0ab85a194696ac12ff153c7f3c014c7a1c15553
Matt Martindale <martindalematt18@yahoo.com>
2020-05-03 19:22:20 -0600

    added addmovie button

---
+++ MyMovies/Views/MovieSearchTableViewCell.swift
@@ -0,0 +1,19 @@
+//
+//  MovieSearchTableViewCell.swift
+//  MyMovies
+//
+//  Created by Matthew Martindale on 5/3/20.
+//  Copyright © 2020 Lambda School. All rights reserved.
+//
+
+import UIKit
+
+class MovieSearchTableViewCell: UITableViewCell {
+
+    @IBOutlet weak var movieLabel: UILabel!
+    @IBAction func addMovieButtonTapped(_ sender: UIButton) {
+        print("hello")
+    }
+    
+
+}
