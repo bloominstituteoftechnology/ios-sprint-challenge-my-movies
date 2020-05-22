89290281b01504b6bcace3ecf472fc461030e96d
kelson99 <kelson99hartle@icloud.com>
2020-05-22 09:25:46 -0600

    set up Core data essentials

---
+++ MyMovies/MovieTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  MovieTableViewCell.swift
+//  MyMovies
+//
+//  Created by Kelson Hartle on 5/22/20.
+//  Copyright © 2020 Lambda School. All rights reserved.
+//
+
+import UIKit
+
+class MovieTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
