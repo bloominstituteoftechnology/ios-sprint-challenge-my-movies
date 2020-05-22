abb8f96e0b27c5aef87764c865ec1fd9b63032bc
Nonye <nonye@Nonyes-MacBook-Air.local>
2020-05-22 08:35:02 -0700

    mymovie & moviecell table view cells created

---
+++ MyMovies/Storyboards/MyMoviesCell.swift
@@ -0,0 +1,24 @@
+//
+//  MyMoviesCell.swift
+//  MyMovies
+//
+//  Created by Nonye on 5/22/20.
+//  Copyright © 2020 Lambda School. All rights reserved.
+//
+
+import UIKit
+
+class MyMoviesCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
