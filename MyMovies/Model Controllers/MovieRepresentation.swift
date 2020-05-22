8d9fa9e2a899271c452abac69717c1d69aa0782a
Nonye <nonye@Nonyes-MacBook-Air.local>
2020-05-22 08:11:13 -0700

    Intial Commit

---
+++ MyMovies/Model Controllers/MovieRepresentation.swift
@@ -0,0 +1,9 @@
+//
+//  MovieRepresentation.swift
+//  MyMovies
+//
+//  Created by Nonye on 5/22/20.
+//  Copyright © 2020 Lambda School. All rights reserved.
+//
+
+import Foundation
---
+++ MyMovies/Model Controllers/Task+Convenience.swift
@@ -0,0 +1,9 @@
+//
+//  Task+Convenience.swift
+//  MyMovies
+//
+//  Created by Nonye on 5/22/20.
+//  Copyright © 2020 Lambda School. All rights reserved.
+//
+
+import Foundation
