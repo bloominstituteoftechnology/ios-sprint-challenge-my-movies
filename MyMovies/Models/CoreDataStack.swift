5f96d45dd3ac21f791d6e2c2f7a7690ae163a670
Enzo Jimenez-Soto <enzojimenezsoto@gmail>
2020-05-22 09:26:39 -0700

    Added files

---
+++ MyMovies/Models/CoreDataStack.swift
@@ -0,0 +1,9 @@
+//
+//  CoreDataStack.swift
+//  MyMovies
+//
+//  Created by Enzo Jimenez-Soto on 5/22/20.
+//  Copyright © 2020 Lambda School. All rights reserved.
+//
+
+import Foundation
---
+++ MyMovies/Models/MovieRepresentation.swift
@@ -0,0 +1,9 @@
+//
+//  MovieRepresentation.swift
+//  MyMovies
+//
+//  Created by Enzo Jimenez-Soto on 5/22/20.
+//  Copyright © 2020 Lambda School. All rights reserved.
+//
+
+import Foundation
