4eca8d846f8a7d4ad02fb16482b7ed60a7d71132
Cody Morley <codymorleyinc@gmail.com>
2020-05-22 09:50:16 -0700

    added a coredata stack for handling persistent stores.

---
+++ MyMovies/Models/CoreData/CoreDataStack.swift
@@ -0,0 +1,29 @@
+//
+//  CoreDataStack.swift
+//  MyMovies
+//
+//  Created by Cody Morley on 5/22/20.
+//  Copyright © 2020 Lambda School. All rights reserved.
+//
+
+import Foundation
+import CoreData
+
+class CoreDataStack {
+    static let shared = CoreDataStack()
+    
+    lazy var container: NSPersistentContainer = {
+        let container = NSPersistentContainer(name: "")
+        container.loadPersistentStores { _, error in
+            if let error = error {
+                fatalError("Failed to load persistent stores: \(error)")
+            }
+        }
+        return container
+    }()
+    
+    var mainContext: NSManagedObjectContext {
+        return container.viewContext
+    }
+}
+
