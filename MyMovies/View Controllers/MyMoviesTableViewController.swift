af966fb6d7e27545a52a37cb1d3cebfb4c53befb
Harmony Radley <harmonyradley99@gmail.com>
2020-05-22 12:57:07 -0400

    Almost MVP

--- MyMovies/View Controllers/MyMoviesTableViewController.swift
+++ MyMovies/View Controllers/MyMoviesTableViewController.swift
@@ -75,10 +75,13 @@ class MyMoviesTableViewController: UITableViewController {
 
     // MARK: - Navigation
 
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+        if segue.identifier == "searchMovieSegue" {
+            if let navC = segue.destination as? UINavigationController,
+                let searchMovieVC = navC.viewControllers.first as? MovieSearchTableViewController {
+                searchMovieVC.movieController = movieController
+            }
+        }
     }
 }
 
