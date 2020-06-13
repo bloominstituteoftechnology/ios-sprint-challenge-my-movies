cf55a29eaff707185b8bc36955b1d65a7f795eef
Clayton Watkins <watkinclayton17@yahoo.com>
2020-06-12 19:37:41 -0700

    Created a custom TableViewCell for our MovieList Table View

--- MyMovies/View Controllers/MovieSearchTableViewController.swift
+++ MyMovies/View Controllers/MovieSearchTableViewController.swift
@@ -21,24 +21,13 @@ class MovieSearchTableViewController: UITableViewController {
         super.viewDidLoad()
         searchBar.delegate = self
     }
-    
-//    override func viewWillDisappear(_ animated: Bool) {
-//        if let indexPaths = tableView.indexPathsForSelectedRows {
-//            for indexPath in indexPaths {
-//                let movieDBMovie = movieController.searchedMovies[indexPath.row]
-//                // TODO: Save this movie representation as a managed object in Core Data
-//            }
-//        }
-//    }
-    
+
     // MARK: - Actions
-    
     @IBAction func doneButtonTapped(_ sender: UIBarButtonItem) {
         navigationController?.dismiss(animated: true, completion: nil)
     }
     
     // MARK: - Table View Data Source
-    
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return movieController.searchedMovies.count
     }
