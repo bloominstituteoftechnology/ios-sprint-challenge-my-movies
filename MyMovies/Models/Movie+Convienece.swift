ed6c162c4a70ac83eb0ccd0903b46756bdd7fd45
Clayton Watkins <watkinclayton17@yahoo.com>
2020-06-12 18:20:33 -0700

    Initial Commit: Set up xcdatamodeled

---
+++ MyMovies/Models/Movie+Convienece.swift
@@ -0,0 +1,19 @@
+//
+//  Movie+Convienece.swift
+//  MyMovies
+//
+//  Created by Clayton Watkins on 6/12/20.
+//  Copyright © 2020 Lambda School. All rights reserved.
+//
+
+import Foundation
+import CoreData
+
+extension Movie {
+    //MARK: - Properties
+    // Creating a computed property for our Movie Representaion
+    
+    
+    
+    
+}
