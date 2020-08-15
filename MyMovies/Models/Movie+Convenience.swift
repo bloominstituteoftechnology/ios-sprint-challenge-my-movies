4a5c84ac5a8341cf7cb06f78085a29ce3b7b98f9
Ronald Huston Jr <rghuston9@gmail.com>
2020-08-15 11:52:17 -0500

    model files

--- MyMovies/Models/Movie+Convenience.swift
+++ MyMovies/Models/Movie+Convenience.swift
@@ -7,3 +7,35 @@
 //
 
 import Foundation
+import CoreData
+
+extension Movie {
+    var movieRepresentation: MovieRepresentation? {
+        guard let title = title,
+            let identifier = identifier else { return nil }
+        
+        return MovieRepresentation(title: title, identifier: identifier.uuidString, hasWatched: hasWatched)
+    }
+    
+    //  to conveniently initialize a movie object
+    @discardableResult convenience init?(title: String,
+                                         identifier: UUID = UUID(),
+                                         hasWatched: Bool = false,
+                                         context: NSManagedObjectContext = CoreDataStack.shared.mainContext) {
+        
+        self.init(context: context)
+        
+        self.title = title
+        self.identifier = identifier
+        self.hasWatched = hasWatched
+    }
+    
+    @discardableResult convenience init?(movieRepresentation: MovieRepresentation, context: NSManagedObjectContext = CoreDataStack.shared.mainContext) {
+        guard let identifier = UUID(uuidString: movieRepresentation.identifier ?? "") else { return nil }
+        
+        self.init(title: movieRepresentation.title,
+                  identifier: identifier,
+                  hasWatched: movieRepresentation.hasWatched,
+                  context: context)
+    }
+}
---
+++ MyMovies/Models/MovieRepresentation.swift
@@ -0,0 +1,19 @@
+//
+//  MovieRepresentation.swift
+//  MyMovies
+//
+//  Created by ronald huston jr on 8/15/20.
+//  Copyright © 2020 Lambda School. All rights reserved.
+//
+
+import Foundation
+
+struct MovieRepresentation: Equatable, Codable {
+    let title: String
+    let identifier: String?
+    let hasWatched: Bool
+}
+
+struct MovieRepresentations: Codable {
+    let results: [MovieRepresentation]
+}
