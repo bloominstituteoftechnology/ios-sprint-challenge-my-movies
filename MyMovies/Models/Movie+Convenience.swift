6f5323734f5f6c09514322fce3c3b2b816c988cd
Eoin Lavery <me@eoinlavery.com>
2020-08-17 03:01:55 +0100

    Completed changed to MovieSearchTableViewController

---
+++ MyMovies/Models/Movie+Convenience.swift
@@ -0,0 +1,29 @@
+//
+//  Movie+Convenience.swift
+//  MyMovies
+//
+//  Created by Eoin Lavery on 17/08/2020.
+//  Copyright © 2020 Lambda School. All rights reserved.
+//
+
+import Foundation
+import CoreData
+
+extension Movie {
+    
+    @discardableResult convenience init(title: String,
+                                        context: NSManagedObjectContext = CoreDataStack.shared.mainContext) {
+        self.init(context: context)
+        self.identifier = UUID()
+        self.title = title
+        self.hasWatched = false
+    }
+    
+    @discardableResult convenience init(movie: MovieRepresentation, context: NSManagedObjectContext = CoreDataStack.shared.mainContext) {
+        
+        let title = movie.title ?? "No Title"
+        
+        self.init(title: title, context: context)
+    }
+    
+}
